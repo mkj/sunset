9c0d6c251dabb0b2b6e22898f509c3eae427177b
Matt Johnston <matt@ucc.asn.au>
2023-03-05 23:21:03 +0800

    Use getrandom directly

--- src/random.rs
+++ src/random.rs
@@ -4,17 +4,9 @@ use {
     log::{debug, error, info, log, trace, warn},
 };
 
-use rand::{RngCore, Rng, CryptoRng};
-use core::num::Wrapping;
-
-pub type SunsetRng = rand::rngs::OsRng;
-
 pub fn fill_random(buf: &mut [u8]) -> Result<(), Error> {
-    // TODO: can this return an error?
-    let mut rng = SunsetRng::default();
-    rng.try_fill_bytes(buf)
-        .map_err(|e| {
-            debug!("RNG failed: {e:?}");
-            Error::msg("RNG failed")
-        })
+    getrandom::getrandom(buf)
+    .map_err(|_| {
+        Error::msg("RNG failed")
+    })
 }
