e1a31b6b553df8432dba38c07c85bd80caf1fa94
Matt Johnston <matt@ucc.asn.au>
2023-10-18 22:59:19 +0800

    Update toolchain, and add script to do it

---
+++ testing/update-toolchain.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp ./rust-toolchain.toml ./embassy/demos/std/rust-toolchain.toml
+cp ./rust-toolchain.toml ./embassy/demos/picow/rust-toolchain.toml
+cp ./rust-toolchain.toml ./embassy/rust-toolchain.toml
+cp ./rust-toolchain.toml ./async/rust-toolchain.toml
