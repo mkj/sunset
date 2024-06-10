mkj/sunset

commit e769b8a4738cdbb545f4abe6512ac4ad73816c1a

Use embassy from crates not git patch

Cargo.toml
@@ -64,7 +64,7 @@ futures = { version = "0.3", default-features = false }
 defmt = { version  = "0.3", optional = true }
 
 [features]
-std = ["snafu/std", "snafu/backtraces", "ssh-key/alloc"]
+std = ["snafu/std", "snafu/backtrace", "ssh-key/alloc"]
 rsa = ["dep:rsa", "ssh-key/rsa"]
 # allows conversion to/from OpenSSH key formats
 openssh-key = ["ssh-key"]
@@ -87,28 +87,6 @@ x25519-dalek = { git = "https://github.com/mkj/curve25519-dalek", branch = "suns
 # curve25519-dalek = { path = "/home/matt/3rd/rs/crypto/curve25519-dalek/curve25519-dalek" }
 # ed25519-dalek = { path = "/home/matt/3rd/rs/crypto/curve25519-dalek/ed25519-dalek" }
 # x25519-dalek = { path = "/home/matt/3rd/rs/crypto/curve25519-dalek/x25519-dalek" }
-
-embassy-executor = { git = "https://github.com/embassy-rs/embassy", rev = "90e96d281f87efdbea3a1418f5354f14386a42e8" }
-embassy-time = { git = "https://github.com/embassy-rs/embassy", rev = "90e96d281f87efdbea3a1418f5354f14386a42e8" }
-# embassy-futures = { git = "https://github.com/embassy-rs/embassy", rev = "90e96d281f87efdbea3a1418f5354f14386a42e8" }
-embassy-rp = { git = "https://github.com/embassy-rs/embassy", rev = "90e96d281f87efdbea3a1418f5354f14386a42e8" }
-embassy-usb = { git = "https://github.com/embassy-rs/embassy", rev = "90e96d281f87efdbea3a1418f5354f14386a42e8" }
-# embassy-sync = { git = "https://github.com/embassy-rs/embassy", rev = "90e96d281f87efdbea3a1418f5354f14386a42e8" }
-embassy-usb-driver = { git = "https://github.com/embassy-rs/embassy", rev = "90e96d281f87efdbea3a1418f5354f14386a42e8" }
-# for cyw43
-embassy-net = { git = "https://github.com/embassy-rs/embassy", rev = "90e96d281f87efdbea3a1418f5354f14386a42e8" }
-embassy-net-driver-channel = { git = "https://github.com/embassy-rs/embassy", rev = "90e96d281f87efdbea3a1418f5354f14386a42e8" }
-embassy-net-driver = { git = "https://github.com/embassy-rs/embassy", rev = "90e96d281f87efdbea3a1418f5354f14386a42e8" }
-embassy-net-tuntap = { git = "https://github.com/embassy-rs/embassy", rev = "90e96d281f87efdbea3a1418f5354f14386a42e8" }
-
-cyw43 = { git = "https://github.com/embassy-rs/embassy/", rev = "90e96d281f87efdbea3a1418f5354f14386a42e8" }
-cyw43-pio = { git = "https://github.com/embassy-rs/embassy/", rev = "90e96d281f87efdbea3a1418f5354f14386a42e8" }
-embassy-net-wiznet = { git = "https://github.com/embassy-rs/embassy/", rev = "90e96d281f87efdbea3a1418f5354f14386a42e8" }
-
-# embassy-net = { path = "/home/matt/3rd/rs/embassy/embassy-net" }
-# embassy-net-driver = { path = "/home/matt/3rd/rs/embassy/embassy-net-driver" }
-# embassy-net-driver-channel = { path = "/home/matt/3rd/rs/embassy/embassy-net-driver-channel" }
-# embassy-net-tuntap = { path = "/home/matt/3rd/rs/embassy/embassy-net-tuntap" }
 
 # these are mostly applicable to picow, but can't hurt generally
 [profile.dev]

embassy/demos/picow/Cargo.toml
@@ -16,17 +16,16 @@ cyw43-pio = { version = "0.1.0", optional = true }
 
 embassy-net-wiznet = { version = "0.1.0", optional = true }
 
-embassy-executor = { version = "0.4",  features = ["defmt", "integrated-timers", "executor-thread", "arch-cortex-m"] }
-embassy-time = { version = "0.2",  features = ["defmt", "defmt-timestamp-uptime"] }
+embassy-executor = { version = "0.5",  features = ["defmt", "integrated-timers", "executor-thread", "arch-cortex-m"] }
+embassy-time = { version = "0.3",  features = ["defmt", "defmt-timestamp-uptime"] }
 embassy-rp = { version = "0.1",  features = ["defmt", "unstable-pac", "time-driver"] }
-# embassy-net/nightly is required for asynch::Read/Write on TcpReader/TcpWriter
-embassy-net = { version = "0.2", features = ["tcp", "dhcpv4", "medium-ethernet"] }
+embassy-net = { version = "0.4", features = ["tcp", "dhcpv4", "medium-ethernet"] }
 embassy-net-driver = { version = "0.2" }
 embassy-usb-driver = { version = "0.1" }
 embassy-sync = { version = "0.5" }
 embassy-futures = { version = "0.1" }
 embassy-usb = { version = "0.1" }
-atomic-polyfill = "0.1.5"
+atomic-polyfill = "1.0"
 static_cell = { version = "1.0", features = [ "nightly" ] }
 
 defmt = { version  = "0.3", optional = true }
@@ -41,11 +40,11 @@ snafu = { version = "0.7", default-features = false, features = ["rust_1_61"] }
 cortex-m = { version = "0.7.6", features = ["critical-section-single-core"]}
 cortex-m-rt = "0.7.0"
 
-embedded-hal-1 = { package = "embedded-hal", version = "1.0.0-alpha.11" }
-embedded-hal-async = { version = "1.0.0-alpha.10" }
+embedded-hal = "1.0"
+embedded-hal-async = "1.0"
 embedded-hal-bus = { version = "0.1.0-rc.1", features = ["async"], optional = true }
 embedded-io-async = "0.6"
-embedded-storage-async = { version = "0.4.0" }
+embedded-storage-async = "0.4"
 heapless = "0.8"
 
 caprand = { git = "https://github.com/mkj/caprand" }

embassy/demos/common/Cargo.toml
@@ -10,10 +10,10 @@ sunset = { path = "../../.." }
 sunset-sshwire-derive = { version = "0.1", path = "../../../sshwire-derive" }
 
 embassy-sync = { version = "0.5" }
-embassy-net = { version = "0.2", features = ["tcp", "dhcpv4", "medium-ethernet"] }
+embassy-net = { version = "0.4", features = ["tcp", "dhcpv4", "medium-ethernet"] }
 embassy-net-driver = { version = "0.2" }
 embassy-futures = { version = "0.1" }
-embassy-time = { version = "0.1" }
+embassy-time = { version = "0.3" }
 
 heapless = "0.8"
 # using local fork

commit 6b7542b62a112873505db0f2aea569c596d2194f

Fix picow demo to work again
It broke when embassy deps were updated without being tested.

NUM_LISTENERS has been reduced to 2 pending debugging issues with
larger arena allocations. "nightly" embassy-executor works OK with
NUM_LISTENERS=4 (or perhaps more).

Fixes: e769b8a ("Use embassy from crates not git patch")

Cargo.toml
@@ -58,9 +58,6 @@ embedded-io = { version = "0.6", optional = true }
 # for debug printing
 pretty-hex = { version = "0.4", default-features = false }
 
-# for non_async
-futures = { version = "0.3", default-features = false }
-
 [features]
 std = ["snafu/std", "snafu/backtrace", "ssh-key/alloc", "larger"]
 rsa = ["dep:rsa", "ssh-key/rsa"]
@@ -85,6 +82,18 @@ simplelog = { version = "0.12", features = ["test"] }
 #ed25519-dalek = { git = "https://github.com/mkj/curve25519-dalek", branch = "sunset" }
 #x25519-dalek = { git = "https://github.com/mkj/curve25519-dalek", branch = "sunset" }
 
+# [patch.crates-io]
+# cyw43 = { path = "/home/matt/3rd/rs/embassy-stable-sunset/cyw43" }
+# embassy-time = { path = "/home/matt/3rd/rs/embassy-stable-sunset/embassy-time" }
+# embassy-time-driver = { path = "/home/matt/3rd/rs/embassy-stable-sunset/embassy-time-driver" }
+# embassy-time-queue-driver = { path = "/home/matt/3rd/rs/embassy-stable-sunset/embassy-time-queue-driver" }
+# embassy-sync = { path = "/home/matt/3rd/rs/embassy-stable-sunset/embassy-sync" }
+# embassy-net = { path = "/home/matt/3rd/rs/embassy-stable-sunset/embassy-net" }
+# embassy-net-driver = { path = "/home/matt/3rd/rs/embassy-stable-sunset/embassy-net-driver" }
+# embassy-net-driver-channel = { path = "/home/matt/3rd/rs/embassy-stable-sunset/embassy-net-driver-channel" }
+# embassy-executor = { path = "/home/matt/3rd/rs/embassy-stable-sunset/embassy-executor" }
+# embassy-executor-macros = { path = "/home/matt/3rd/rs/embassy-stable-sunset/embassy-executor-macros" }
+
 # these are mostly applicable to picow, but can't hurt generally
 [profile.dev]
 debug = 2
@@ -97,9 +106,10 @@ codegen-units = 1
 debug = 2
 debug-assertions = false
 incremental = false
-lto = 'fat'
+lto = "fat"
 opt-level = 'z'
 overflow-checks = false
+panic = "abort"
 
 # do not optimize proc-macro crates = faster builds from scratch
 [profile.dev.build-override]

embassy/demos/common/src/lib.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(not(any(feature = "std", test)), no_std)]
+#![no_std]
 
 // avoid warning about Send for the time being
 #[allow(async_fn_in_trait)]

embassy/demos/common/src/server.rs
@@ -28,9 +28,9 @@ pub async fn listener<D: Driver, S: DemoServer>(stack: &'static Stack<D>,
     let mut rx_buffer = [0; 1550];
     let mut tx_buffer = [0; 1550];
 
+    let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
+    // socket.set_nagle_enabled(false);
     loop {
-        let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
-        // socket.set_nagle_enabled(false);
 
         info!("Listening on TCP:22...");
         if let Err(_) = socket.accept(22).await {
@@ -159,6 +159,7 @@ impl ServerApp {
         };
 
         if p.check(password) {
+            info!("Password login for {username}");
             a.allow()?
         }
         Ok(())

embassy/demos/picow/.cargo/config.toml
@@ -1,9 +1,6 @@
 [target.'cfg(all(target_arch = "arm", target_os = "none"))']
-runner = "probe-run --chip RP2040"
+runner = "probe-rs  run --chip RP2040 --speed 20000"
 
 [build]
 target = "thumbv6m-none-eabi"
-rustflags = "-Cllvm-args=--enable-machine-outliner=never"
 
-[env]
-DEFMT_LOG = "debug"

embassy/demos/picow/Cargo.toml
@@ -9,27 +9,33 @@ sunset = { path = "../../.." }
 sunset-sshwire-derive = { path = "../../../sshwire-derive" }
 sunset-demo-embassy-common= { path = "../common" }
 
-cyw43 = { version = "0.1.0", optional = true}
+cyw43 = { version = "0.1.0", optional = true, features = ["log", "firmware-logs"]}
 cyw43-pio = { version = "0.1.0", optional = true }
 
 embassy-net-wiznet = { version = "0.1.0", optional = true }
 
-embassy-executor = { version = "0.5",  features = ["integrated-timers", "executor-thread", "arch-cortex-m"] }
+embassy-executor = { version = "0.5", features = [
+    "integrated-timers", "executor-thread", "arch-cortex-m", "log",
+    # Larger than around 120kB causes unknown failures. using "nightly" feature
+    # instead works OK. Needs investigation. 96kB is sufficient for NUM_LISTENERS=2
+    # https://github.com/embassy-rs/embassy/issues/3061
+    "task-arena-size-98304"] }
 embassy-time = { version = "0.3",  features = [] }
 embassy-rp = { version = "0.1",  features = ["unstable-pac", "time-driver"] }
-embassy-net = { version = "0.4", features = ["tcp", "dhcpv4", "medium-ethernet"] }
+embassy-net = { version = "0.4", features = ["tcp", "dhcpv4", "medium-ethernet", "log"] }
 embassy-net-driver = { version = "0.2" }
 embassy-usb-driver = { version = "0.1" }
 embassy-sync = { version = "0.5" }
 embassy-futures = { version = "0.1" }
-embassy-usb = { version = "0.1" }
+embassy-usb = { version = "0.1", features = ["log"] }
 atomic-polyfill = "1.0"
 static_cell = { version = "1.0", features = [ "nightly" ] }
 
-panic-probe = { version = "0.3", features = [] }
-pretty-hex = { version = "0.4", default-features = false }
 log = { version = "0.4" }
-futures = { version = "0.3", default-features = false }
+rtt-target = "0.3"
+rtt-logger = "0.2"
+
+pretty-hex = { version = "0.4", default-features = false }
 
 snafu = { version = "0.8", default-features = false, features = ["rust_1_65"] }
 
@@ -61,8 +67,8 @@ cyw43 = ["dep:cyw43", "dep:cyw43-pio"]
 w5500 = ["dep:embassy-net-wiznet", "dep:embedded-hal-bus"]
 
 # Use cyw43 firmware already on flash. This saves time when developing.
-# probe-rs-cli download firmware/43439A0.bin --format bin --chip RP2040 --base-address 0x10100000
-# probe-rs-cli download firmware/43439A0_clm.bin --format bin --chip RP2040 --base-address 0x10140000
+# probe-rs download firmware/43439A0.bin --binary-format bin  --chip RP2040 --base-address 0x10100000
+# probe-rs download firmware/43439A0_clm.bin --binary-format bin  --chip RP2040 --base-address 0x10140000
 romfw = []
 
 # Set default console to serial

embassy/demos/picow/memory.x
@@ -1,5 +1,5 @@
 MEMORY {
     BOOT2 : ORIGIN = 0x10000000, LENGTH = 0x100
-    FLASH : ORIGIN = 0x10000100, LENGTH = 1024K - 0x100
-    RAM   : ORIGIN = 0x20000000, LENGTH = 256K
+    FLASH : ORIGIN = 0x10000100, LENGTH = 2048K - 0x100
+    RAM   : ORIGIN = 0x20000000, LENGTH = 264K
 }

embassy/demos/picow/src/flashconfig.rs
@@ -37,8 +37,9 @@ struct FlashConfig<'a> {
 }
 
 impl FlashConfig<'_> {
-    const BUF_SIZE: usize = 1 + SSHConfig::BUF_SIZE + 32;
+    const BUF_SIZE: usize = 4 + SSHConfig::BUF_SIZE + 32;
 }
+const _: () = assert!(FlashConfig::BUF_SIZE % 4 == 0, "flash reads must be a multiple of 4");
 
 fn config_hash(config: &SSHConfig) -> Result<[u8; 32]> {
     let mut h = sha2::Sha256::new();
@@ -71,7 +72,10 @@ pub async fn create(flash: &mut Fl<'_>) -> Result<SSHConfig> {
 pub async fn load(flash: &mut Fl<'_>) -> Result<SSHConfig> {
     // let mut buf = [0u8; ERASE_SIZE];
     let mut buf = [0u8; FlashConfig::BUF_SIZE];
-    flash.read(CONFIG_OFFSET, &mut buf).await.map_err(|_| Error::msg("flash error"))?;
+    flash.read(CONFIG_OFFSET, &mut buf).await.map_err(|e| {
+        debug!("flash read error 0x{CONFIG_OFFSET:x} {e:?}");
+        Error::msg("flash error")
+    })?;
 
     // use pretty_hex::PrettyHex;
     // use core::fmt::Write;

embassy/demos/picow/src/main.rs
@@ -6,8 +6,6 @@ pub use log::{debug, error, info, log, trace, warn};
 
 use core::ops::ControlFlow;
 
-use {panic_probe as _};
-
 use embassy_executor::Spawner;
 use embassy_futures::select::{select, Either};
 use embassy_net::{Stack, HardwareAddress, EthernetAddress};
@@ -30,7 +28,7 @@ mod flashconfig;
 mod picowmenu;
 mod serial;
 mod usb;
-mod keyboard;
+// mod keyboard;
 #[cfg(feature = "w5500")]
 mod w5500;
 #[cfg(feature = "cyw43")]
@@ -42,13 +40,25 @@ compile_error!("No network device selected. Use cyw43 or w5500 feature");
 #[cfg(all(feature = "cyw43", feature = "w5500"))]
 compile_error!("Select only one of cyw43 or w5500");
 
-const NUM_LISTENERS: usize = 4;
+const NUM_LISTENERS: usize = 2;
 // +1 for dhcp. referenced directly by wifi_stack() function
 pub(crate) const NUM_SOCKETS: usize = NUM_LISTENERS + 1;
 
+const LOG_LEVEL: log::LevelFilter = log::LevelFilter::Debug;
+static LOGGER: rtt_logger::RTTLogger = rtt_logger::RTTLogger::new(LOG_LEVEL);
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
+    // Some logs are better than fully dropped. Use a larger buffer.
+    rtt_target::rtt_init_print!(NoBlockTrim, 4000);
+    // rtt_target::rtt_init_print!(BlockIfFull);
+    unsafe {
+        log::set_logger_racy(&LOGGER).unwrap();
+        log::set_max_level_racy(LOG_LEVEL);
+    }
     info!("Welcome to Sunset SSH");
+    debug!("Debug");
+    trace!("Trace");
 
     let mut p = embassy_rp::init(Default::default());
 
@@ -84,16 +94,6 @@ async fn main(spawner: Spawner) {
         let p = SERS.init(Default::default());
         SERP.init_with(|| p.build())
     };
-    spawner
-        .spawn(serial::task(
-            p.UART0,
-            p.PIN_0,
-            p.PIN_1,
-            p.PIN_2,
-            p.PIN_3,
-            serial1_pipe,
-        ))
-        .unwrap();
 
     // Watchdog currently only used for triggering reset manually
     static WD: StaticCell<SunsetMutex<embassy_rp::watchdog::Watchdog>> = StaticCell::new();
@@ -101,11 +101,12 @@ async fn main(spawner: Spawner) {
         embassy_rp::watchdog::Watchdog::new(p.WATCHDOG)
     ));
 
+    // state depends on the mac for cyw43 vs w5500
+    let net_mac = SunsetMutex::new([0; 6]);
     static STATE: StaticCell<GlobalState> = StaticCell::new();
-    let state;
+    let state = STATE.init_with(|| GlobalState { usb_pipe, serial1_pipe, config, flash, watchdog, net_mac});
 
     // Spawn network tasks to handle incoming connections with demo_common::session()
-
     #[cfg(feature = "cyw43")]
     {
         let stack = wifi::wifi_stack(
@@ -114,10 +115,8 @@ async fn main(spawner: Spawner) {
         )
         .await;
 
-        let net_mac = match stack.hardware_address() {
-            HardwareAddress::Ethernet(EthernetAddress(eth)) => eth,
-        };
-        state = STATE.init_with(|| GlobalState { usb_pipe, serial1_pipe, config, flash, watchdog, net_mac });
+        let HardwareAddress::Ethernet(EthernetAddress(eth)) = stack.hardware_address();
+        *state.net_mac.lock().await = eth;
         for _ in 0..NUM_LISTENERS {
             spawner.spawn(cyw43_listener(&stack, config, state)).unwrap();
         }
@@ -132,22 +131,29 @@ async fn main(spawner: Spawner) {
         )
         .await;
 
-        let net_mac = match stack.hardware_address() {
-            HardwareAddress::Ethernet(EthernetAddress(eth)) => eth,
-        };
-        state = STATE.init_with(|| GlobalState { usb_pipe, serial1_pipe, config, flash, watchdog, net_mac });
+        let HardwareAddress::Ethernet(EthernetAddress(eth)) = stack.hardware_address();
+        *state.net_mac.lock().await = eth;
         for _ in 0..NUM_LISTENERS {
             spawner.spawn(w5500_listener(&stack, config, state)).unwrap();
         }
     }
 
-    // USB task requires `state`
+    spawner
+        .spawn(serial::task(
+            p.UART0,
+            p.PIN_0,
+            p.PIN_1,
+            p.PIN_2,
+            p.PIN_3,
+            serial1_pipe,
+        ))
+        .unwrap();
+
     spawner.spawn(usb::task(p.USB, state)).unwrap();
 }
 
-// TODO: pool_size should be NUM_LISTENERS but needs a literal
 #[cfg(feature = "cyw43")]
-#[embassy_executor::task(pool_size = 4)]
+#[embassy_executor::task(pool_size = NUM_LISTENERS)]
 async fn cyw43_listener(
     stack: &'static Stack<cyw43::NetDriver<'static>>,
     config: &'static SunsetMutex<SSHConfig>,
@@ -157,7 +163,7 @@ async fn cyw43_listener(
 }
 
 #[cfg(feature = "w5500")]
-#[embassy_executor::task(pool_size = 4)]
+#[embassy_executor::task(pool_size = NUM_LISTENERS)]
 async fn w5500_listener(
     stack: &'static Stack<embassy_net_wiznet::Device<'static>>,
     config: &'static SunsetMutex<SSHConfig>,
@@ -167,7 +173,7 @@ async fn w5500_listener(
 }
 
 pub(crate) struct GlobalState {
-    // If locking multiple mutexes, always lock in the order below avoid inversion.
+    // If locking multiple mutexes, always lock in the order below to avoid inversion.
     pub usb_pipe: &'static TakePipe<'static>,
     pub serial1_pipe: &'static TakePipe<'static>,
 
@@ -175,7 +181,7 @@ pub(crate) struct GlobalState {
     pub flash: &'static SunsetMutex<flashconfig::Fl<'static>>,
     pub watchdog: &'static SunsetMutex<embassy_rp::watchdog::Watchdog>,
 
-    pub net_mac: [u8; 6],
+    pub net_mac: SunsetMutex<[u8; 6]>,
 }
 
 struct PicoServer {
@@ -290,7 +296,6 @@ where
     info!("serial task completed");
     Ok(())
 }
-
 impl DemoServer for PicoServer {
     type Init = &'static GlobalState;
 
@@ -366,5 +371,13 @@ impl DemoServer for PicoServer {
             Either::First(r) => r,
             Either::Second(r) => r,
         }
+    }
+}
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    error!("{}", info);
+    loop {
+        cortex_m::asm::wfi();
     }
 }

embassy/demos/picow/src/picowmenu.rs
@@ -733,7 +733,6 @@ fn do_net_info(_item: &Item<MenuCtx>, _args: &[&str], context: &mut MenuCtx) {
     let _ = writeln!(context.out, "w5500 wired ethernet");
     #[cfg(feature = "cyw43")]
     let _ = writeln!(context.out, "cyw43 wifi");
-    let _ = writeln!(context.out, "mac address {}", Mac(context.state.net_mac));
 
     context.with_config(|c, out| {
         if let Some(ref stat) = c.ip4_static {

embassy/demos/picow/src/usb.rs
@@ -1,15 +1,15 @@
 #[allow(unused_imports)]
 pub use log::{debug, error, info, log, trace, warn};
 
-use embassy_futures::join::{join, join4};
+use embassy_futures::join::{join, join3};
 use embassy_rp::bind_interrupts;
 use embassy_rp::peripherals::USB;
 use embassy_rp::usb::InterruptHandler;
 use embassy_usb::class::cdc_acm::{self, CdcAcmClass};
-use embassy_usb::class::hid::{self, HidReaderWriter};
+// use embassy_usb::class::hid::{self, HidReaderWriter};
 use embassy_usb::Builder;
 use embassy_usb_driver::Driver;
-use usbd_hid::descriptor::{KeyboardReport, SerializedDescriptor};
+// use usbd_hid::descriptor::{KeyboardReport, SerializedDescriptor};
 
 use embedded_io_async::{Read, Write, BufRead, ErrorType};
 
@@ -54,7 +54,7 @@ pub(crate) async fn task(
     // lives longer than builder
     let mut usb_state0 = cdc_acm::State::new();
     let mut usb_state2 = cdc_acm::State::new();
-    let mut usb_state4 = hid::State::new();
+    // let mut usb_state4 = hid::State::new();
 
     let mut builder = Builder::new(
         driver,
@@ -71,14 +71,14 @@ pub(crate) async fn task(
     // if02
     let cdc2 = CdcAcmClass::new(&mut builder, &mut usb_state2, 64);
 
-    let hid_config = embassy_usb::class::hid::Config {
-        report_descriptor: KeyboardReport::desc(),
-        request_handler: None,
-        poll_ms: 20,
-        max_packet_size: 64,
-    };
-    let hid =
-        HidReaderWriter::<_, 1, 8>::new(&mut builder, &mut usb_state4, hid_config);
+    // let hid_config = embassy_usb::class::hid::Config {
+    //     report_descriptor: KeyboardReport::desc(),
+    //     request_handler: None,
+    //     poll_ms: 20,
+    //     max_packet_size: 64,
+    // };
+    // let hid =
+    //     HidReaderWriter::<_, 1, 8>::new(&mut builder, &mut usb_state4, hid_config);
 
     let mut usb = builder.build();
 
@@ -92,9 +92,10 @@ pub(crate) async fn task(
     let io2_run = menu_if02_run(&global, cdc2);
 
     // keyboard
-    let hid_run = keyboard::run(&global, hid);
+    // let hid_run = keyboard::run(&global, hid);
 
-    join4(usb_fut, io0_run, io2_run, hid_run).await;
+    // join4(usb_fut, io0_run, io2_run, hid_run).await;
+    join3(usb_fut, io0_run, io2_run).await;
     unreachable!()
 }
 
@@ -127,13 +128,17 @@ async fn menu_if02_run<'a, D: Driver<'a>>(
 ) -> ! {
     let (mut cdc_tx, mut cdc_rx) = cdc.split();
     'usb: loop {
+        debug!("wait menu if02 conn");
         cdc_rx.wait_connection().await;
+        debug!("got menu if02 conn");
         let mut cdc_tx = CDCWrite::new(&mut cdc_tx);
         let mut cdc_rx = CDCRead::new(&mut cdc_rx);
+
 
         // wait for a keystroke before writing anything.
         let mut c = [0u8];
         let _ = cdc_rx.read_exact(&mut c).await;
+        debug!("read {:02x}", c[0]);
 
         let p = {
             let c = global.config.lock().await;

embassy/demos/picow/src/wifi.rs
@@ -43,9 +43,8 @@ pub(crate) async fn wifi_stack(spawner: &Spawner,
     config: &'static SunsetMutex<SSHConfig>,
     ) -> &'static embassy_net::Stack<cyw43::NetDriver<'static>>
     {
-    // TODO: return `control` once it can do something useful
 
-    let (fw, clm) = get_fw();
+    let (fw, _clm) = get_fw();
 
     let pwr = Output::new(p23, Level::Low);
     let cs = Output::new(p25, Level::High);
@@ -54,21 +53,50 @@ pub(crate) async fn wifi_stack(spawner: &Spawner,
 
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
     let state = STATE.init_with(|| cyw43::State::new());
-    let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    let (net_device, control, runner) = cyw43::new(state, pwr, spi, fw).await;
     spawner.spawn(wifi_task(runner)).unwrap();
 
+    let seed = OsRng.next_u64();
+    let net_cf = if let Some(ref s) = config.lock().await.ip4_static {
+        embassy_net::Config::ipv4_static(s.clone())
+    } else {
+        embassy_net::Config::dhcpv4(Default::default())
+    };
+
+    static SR: StaticCell<StackResources::<{crate::NUM_SOCKETS}>> = StaticCell::new();
+
+    // Init network stack
+    static STACK: StaticCell<Stack<cyw43::NetDriver>> = StaticCell::new();
+    let stack = STACK.init_with(|| Stack::new(
+        net_device,
+        net_cf,
+        SR.init_with(|| StackResources::new()),
+        seed)
+    );
+
+    spawner.spawn(net_task(stack, control, config)).unwrap();
+
+    stack
+}
+
+#[embassy_executor::task]
+async fn net_task(stack: &'static Stack<cyw43::NetDriver<'static>>,
+    mut control: cyw43::Control<'static>,
+    config: &'static SunsetMutex<SSHConfig>,
+    ) -> ! {
+    let (_fw, clm) = get_fw();
+    // control init() must occur before the net stack tries to access cyw43, otherwise
+    // it seems to get stuck. await it here before spawning the net task.
     control.init(clm).await;
+
     // the default is PowerSave. None is fastest.
-    // control.set_power_management(cyw43::PowerManagementMode::None).await;
-    // control.set_power_management(cyw43::PowerManagementMode::Performance).await;
+    control.set_power_management(cyw43::PowerManagementMode::None).await;
 
     let (wifi_net, wifi_pw) = {
         let c = config.lock().await;
         (c.wifi_net.clone(), c.wifi_pw.clone())
     };
 
-    // TODO: this should move out of the critical path, run in the bg.
-    // just return control before joining.
     for _ in 0..2 {
         let status = if let Some(ref pw) = wifi_pw {
             info!("wifi net {} wpa2", wifi_net);
@@ -77,39 +105,15 @@ pub(crate) async fn wifi_stack(spawner: &Spawner,
             info!("wifi net {} open", wifi_net);
             control.join_open(&wifi_net).await
         };
+
         if let Err(ref e) = status {
             info!("wifi join failed, code {}", e.status);
         } else {
+            info!("wifi joined");
             break;
         }
     }
 
-    let config = if let Some(ref s) = config.lock().await.ip4_static {
-        embassy_net::Config::ipv4_static(s.clone())
-    } else {
-        embassy_net::Config::dhcpv4(Default::default())
-    };
-
-    let seed = OsRng.next_u64();
-
-    static SR: StaticCell<StackResources::<{crate::NUM_SOCKETS}>> = StaticCell::new();
-
-    // Init network stack
-    static STACK: StaticCell<Stack<cyw43::NetDriver>> = StaticCell::new();
-    let stack = STACK.init_with(|| Stack::new(
-        net_device,
-        config,
-        SR.init_with(|| StackResources::new()),
-        seed)
-    );
-
-    spawner.spawn(net_task(stack)).unwrap();
-
-    stack
-}
-
-#[embassy_executor::task]
-async fn net_task(stack: &'static Stack<cyw43::NetDriver<'static>>) -> ! {
     stack.run().await
 }
 
@@ -122,8 +126,10 @@ fn get_fw() -> (&'static [u8], &'static [u8]) {
 
     // To make flashing faster for development, you may want to flash the firmwares independently
     // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
-    //     probe-rs-cli download 43439A0.bin --format bin --chip RP2040 --base-address 0x10100000
-    //     probe-rs-cli download 43439A0_clm.bin --format bin --chip RP2040 --base-address 0x10140000
+    /*
+       probe-rs download firmware/43439A0.bin --binary-format bin  --chip RP2040 --base-address 0x10100000
+       probe-rs download firmware/43439A0_clm.bin --binary-format bin  --chip RP2040 --base-address 0x10140000
+    */
     #[cfg(feature = "romfw")]
     let (fw, clm) = (
         unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, fw.len()) },

embassy/src/embassy_sunset.rs
@@ -138,10 +138,10 @@ impl<'a> EmbassySunset<'a> {
         let rx_stop = Signal::<SunsetRawMutex, ()>::new();
 
         let tx = async {
+            let mut buf = [0; 1024];
             loop {
                 // TODO: make sunset read directly from socket, no intermediate buffer?
                 // Perhaps not possible async, might deadlock.
-                let mut buf = [0; 1024];
                 let l = self.output(&mut buf).await?;
                 if wsock.write_all(&buf[..l]).await.is_err() {
                     info!("socket write error");
@@ -154,9 +154,9 @@ impl<'a> EmbassySunset<'a> {
         let tx = select(tx, tx_stop.wait());
 
         let rx = async {
+            let mut buf = [0; 1024];
             loop {
                 // TODO: make sunset read directly from socket, no intermediate buffer.
-                let mut buf = [0; 1024];
                 let l = match rsock.read(&mut buf).await {
                     Ok(0) => {
                         debug!("net EOF");
@@ -299,7 +299,7 @@ impl<'a> EmbassySunset<'a> {
         *guard = None;
 
         #[cfg(not(feature = "try-polonius"))]
-        let guardptr = guard as *mut Option<MutexGuard<'g, SunsetRawMutex, Inner<'a>>>;
+        let guardptr = guard as *mut Option<_>;
 
         // poll progress until we get an actual event to return
         loop {

src/channel.rs
@@ -854,15 +854,13 @@ impl core::fmt::Display for ChanNum {
     }
 }
 
-/// Channel data packet type.
-///
-/// The SSH specification allows other `u32` types, though Sunset doesn't
-/// currently implement it, they are not widely used.
+/// Channel data type, normal or stderr
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub enum ChanData {
     /// `SSH_MSG_CHANNEL_DATA`
     Normal,
-    /// `SSH_MSG_CHANNEL_EXTENDED_DATA`
+    /// `SSH_MSG_CHANNEL_EXTENDED_DATA`. Only `Stderr` is implemented by Sunset,
+    /// other types are not widely used.
     Stderr,
     // Future API:
     // Other(u32),

src/lib.rs
@@ -20,7 +20,6 @@ pub mod config;
 // exported so that some Channel error variants can be created with .fail().
 // perhaps the ones of interest should be expored separately.
 pub mod error;
-// perhaps don't need this, users could just use getrandom?
 pub mod random;
 
 pub mod event;

src/sshnames.rs
@@ -1,8 +1,8 @@
 //! Named SSH algorithms, methods, and extensions.
 //!
 //! Some identifiers are also listed directly in `packet.rs` derive attributes,
-//! for example [channel request identifiers](packets::ChannelReqType).
-//! Packet numbers are listed in `packets.rs`.
+//! for example [channel request identifiers](crate::packets::ChannelReqType).
+//! [Packet numbers](crate::packets::MessageNumber) are listed in `packets.rs`.
 //!
 //! This module also serves as an index of SSH specifications.
 