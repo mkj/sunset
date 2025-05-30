ab9a9bc80d28b5998cedd8a1d8a3a1068330d6ef
Matt Johnston <matt@ucc.asn.au>
2025-05-30 23:45:52 +0800

    demo tap.sh: set link up, make executable

--- embassy/demos/std/tap.sh
+++ embassy/demos/std/tap.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 # This script generates the tap device that the demo will bind the network stack
-# usage `sudo sh tap.sh`
+# usage `sudo ./tap.sh`
 
 ip tuntap add name tap0 mode tap user $SUDO_USER group $SUDO_USER
 ip addr add 192.168.69.100/24 dev tap0
+ip link set tap0 up
