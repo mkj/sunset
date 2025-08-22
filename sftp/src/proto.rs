d193e090357af2cb513eefdf79cc4b8e9a99aaff
jubeormk1 <jubeormk1@gmail.com>
2025-08-22 11:07:58 +1000

    commenting out sftpmessages macro rules to focus on the foundational SFTP data types

--- sftp/src/proto.rs
+++ sftp/src/proto.rs
@@ -154,207 +154,206 @@ impl From<Error> for SunsetError {
     }
 }
 
-macro_rules! sftpmessages {
-    (
-        $( ( $message_num:literal,
-            $SpecificPacketVariant:ident,
-            $SpecificPacketType:ty,
-            $SSH_FXP_NAME:ident
-            ),
-             )*
-    ) => {
+// macro_rules! sftpmessages {
+//     (
+//         $( ( $message_num:literal,
+//             $SpecificPacketVariant:ident,
+//             $SpecificPacketType:ty,
+//             $SSH_FXP_NAME:ident
+//             ),
+//              )*
+//     ) => {
 
+// #[derive(Debug, Clone)]
+// #[repr(u8)]
+// #[allow(non_camel_case_types)]
+// pub enum SftpNum {
+//     // variants are eg
+//     // SSH_FXP_OPEN = 3,
+//     $(
+//     $SSH_FXP_NAME = $message_num,
+//     )*
+// }
 
-#[derive(Debug, Clone)]
-#[repr(u8)]
-#[allow(non_camel_case_types)]
-pub enum SftpNum {
-    // variants are eg
-    // SSH_FXP_OPEN = 3,
-    $(
-    $SSH_FXP_NAME = $message_num,
-    )*
-}
+// impl SftpNum {
+//     fn is_request(&self) -> bool {
+//         // TODO SSH_FXP_EXTENDED
+//         (2..=99).contains(&(self.clone() as u8))
+//     }
 
-impl SftpNum {
-    fn is_request(&self) -> bool {
-        // TODO SSH_FXP_EXTENDED
-        (2..=99).contains(&(self.clone() as u8))
-    }
+//     fn is_response(&self) -> bool {
+//         // TODO SSH_FXP_EXTENDED_REPLY
+//         (100..=199).contains(&(self.clone() as u8))
+//     }
+// }
 
-    fn is_response(&self) -> bool {
-        // TODO SSH_FXP_EXTENDED_REPLY
-        (100..=199).contains(&(self.clone() as u8))
-    }
-}
+// impl TryFrom<u8> for SftpNum {
+//     type Error = Error;
+//     fn try_from(v: u8) -> Result<Self> {
+//         match v {
+//             // eg
+//             // 3 => Ok(SftpNum::SSH_FXP_OPEN)
+//             $(
+//             $message_num => Ok(SftpNum::$SSH_FXP_NAME),
+//             )*
+//             _ => {
+//                 Err(Error::UnknownPacket { number: v })
+//             }
+//         }
+//     }
+// }
 
-impl TryFrom<u8> for SftpNum {
-    type Error = Error;
-    fn try_from(v: u8) -> Result<Self> {
-        match v {
-            // eg
-            // 3 => Ok(SftpNum::SSH_FXP_OPEN)
-            $(
-            $message_num => Ok(SftpNum::$SSH_FXP_NAME),
-            )*
-            _ => {
-                Err(Error::UnknownPacket { number: v })
-            }
-        }
-    }
-}
+// impl SSHEncode for SftpPacket<'_> {
+//     fn enc(&self, s: &mut dyn SSHSink) -> WireResult<()> {
+//         let t = self.message_num() as u8;
+//         t.enc(s)?;
+//         match self {
+//             // eg
+//             // Packet::KexInit(p) => {
+//             // ...
+//             $(
+//             Packet::$SpecificPacketVariant(p) => {
+//                 p.enc(s)?
+//             }
+//             )*
+//         };
+//         Ok(())
+//     }
+// }
 
-impl SSHEncode for SftpPacket<'_> {
-    fn enc(&self, s: &mut dyn SSHSink) -> WireResult<()> {
-        let t = self.message_num() as u8;
-        t.enc(s)?;
-        match self {
-            // eg
-            // Packet::KexInit(p) => {
-            // ...
-            $(
-            Packet::$SpecificPacketVariant(p) => {
-                p.enc(s)?
-            }
-            )*
-        };
-        Ok(())
-    }
-}
+// impl<'de: 'a, 'a> SSHDecode<'de> for SftpPacket<'a> {
+//     fn dec<S>(s: &mut S) -> WireResult<Self>
+//     where S: SSHSource<'de> {
+//         let msg_num = u8::dec(s)?;
+//         let ty = MessageNumber::try_from(msg_num);
+//         let ty = match ty {
+//             Ok(t) => t,
+//             Err(_) => return Err(WireError::UnknownPacket { number: msg_num })
+//         };
 
-impl<'de: 'a, 'a> SSHDecode<'de> for SftpPacket<'a> {
-    fn dec<S>(s: &mut S) -> WireResult<Self>
-    where S: SSHSource<'de> {
-        let msg_num = u8::dec(s)?;
-        let ty = MessageNumber::try_from(msg_num);
-        let ty = match ty {
-            Ok(t) => t,
-            Err(_) => return Err(WireError::UnknownPacket { number: msg_num })
-        };
+//         // Decode based on the message number
+//         let p = match ty {
+//             // eg
+//             // MessageNumber::SSH_MSG_KEXINIT => Packet::KexInit(
+//             // ...
+//             $(
+//             MessageNumber::$SSH_FXP_NAME => Packet::$SpecificPacketVariant(SSHDecode::dec(s)?),
+//             )*
+//         };
+//         Ok(p)
+//     }
+// }
 
-        // Decode based on the message number
-        let p = match ty {
-            // eg
-            // MessageNumber::SSH_MSG_KEXINIT => Packet::KexInit(
-            // ...
-            $(
-            MessageNumber::$SSH_FXP_NAME => Packet::$SpecificPacketVariant(SSHDecode::dec(s)?),
-            )*
-        };
-        Ok(p)
-    }
-}
+// /// Top level SSH packet enum
+// #[derive(Debug)]
+// pub enum SftpPacket<'a> {
+//     // eg Open(Open<'a>),
+//     $(
+//     $SpecificPacketVariant($SpecificPacketType),
+//     )*
+// }
 
-/// Top level SSH packet enum
-#[derive(Debug)]
-pub enum SftpPacket<'a> {
-    // eg Open(Open<'a>),
-    $(
-    $SpecificPacketVariant($SpecificPacketType),
-    )*
-}
+// impl<'a> SftpPacket<'a> {
+//     pub fn sftp_num(&self) -> SftpNum {
+//         match self {
+//             // eg
+//             // SftpPacket::Open(_) => {
+//             // ..
+//             $(
+//             SftpPacket::$SpecificPacketVariant(_) => {
+//                 MessageNumber::$SSH_FXP_NAME
+//             }
+//             )*
+//         }
+//     }
 
-impl<'a> SftpPacket<'a> {
-    pub fn sftp_num(&self) -> SftpNum {
-        match self {
-            // eg
-            // SftpPacket::Open(_) => {
-            // ..
-            $(
-            SftpPacket::$SpecificPacketVariant(_) => {
-                MessageNumber::$SSH_FXP_NAME
-            }
-            )*
-        }
-    }
+//     /// Encode a request.
+//     ///
+//     /// Used by a SFTP client. Does not include the length field.
+//     pub fn encode_request(&self, id: ReqId, s: &mut dyn SSHSink) -> Result<()> {
+//         if !self.sftp_num().is_request() {
+//             return Err(Error::bug())
+//         }
 
-    /// Encode a request.
-    ///
-    /// Used by a SFTP client. Does not include the length field.
-    pub fn encode_request(&self, id: ReqId, s: &mut dyn SSHSink) -> Result<()> {
-        if !self.sftp_num().is_request() {
-            return Err(Error::bug())
-        }
+//         // packet type
+//         self.sftp_num().enc(s)?;
+//         // request ID
+//         id.0.enc(s)?;
+//         // contents
+//         self.enc(s)
+//     }
 
-        // packet type
-        self.sftp_num().enc(s)?;
-        // request ID
-        id.0.enc(s)?;
-        // contents
-        self.enc(s)
-    }
+//     /// Decode a response.
+//     ///
+//     /// Used by a SFTP client. Does not include the length field.
+//     pub fn decode_response(s: &mut dyn SSHSource) -> WireResult<(ReqId, Self)> {
+//         let num = SftpNum::try_from(u8::dec(s)?)?;
 
-    /// Decode a response.
-    ///
-    /// Used by a SFTP client. Does not include the length field.
-    pub fn decode_response(s: &mut dyn SSHSource) -> WireResult<(ReqId, Self)> {
-        let num = SftpNum::try_from(u8::dec(s)?)?;
+//         if !num.is_response() {
+//             return error::SSHProto.fail();
+//         }
 
-        if !num.is_response() {
-            return error::SSHProto.fail();
-        }
+//         let id = ReqId(u32::dec(s)?);
+//         Ok((id, Self::dec(s)))
+//     }
 
-        let id = ReqId(u32::dec(s)?);
-        Ok((id, Self::dec(s)))
-    }
+//     /// Decode a request.
+//     ///
+//     /// Used by a SFTP server. Does not include the length field.
+//     pub fn decode_request(s: &mut dyn SSHSource) -> WireResult<(ReqId, Self)> {
+//         let num = SftpNum::try_from(u8::dec(s)?)?;
 
-    /// Decode a request.
-    ///
-    /// Used by a SFTP server. Does not include the length field.
-    pub fn decode_request(s: &mut dyn SSHSource) -> WireResult<(ReqId, Self)> {
-        let num = SftpNum::try_from(u8::dec(s)?)?;
+//         if !num.is_request() {
+//             return error::SSHProto.fail();
+//         }
 
-        if !num.is_request() {
-            return error::SSHProto.fail();
-        }
+//         let id = ReqId(u32::dec(s)?);
+//         Ok((id, Self::dec(s)))
+//     }
 
-        let id = ReqId(u32::dec(s)?);
-        Ok((id, Self::dec(s)))
-    }
+//     /// Encode a response.
+//     ///
+//     /// Used by a SFTP server. Does not include the length field.
+//     pub fn encode_response(&self, id: ReqId, s: &mut dyn SSHSink) -> Result<()> {
+//         if !self.sftp_num().is_response() {
+//             return Err(Error::bug())
+//         }
 
-    /// Encode a response.
-    ///
-    /// Used by a SFTP server. Does not include the length field.
-    pub fn encode_response(&self, id: ReqId, s: &mut dyn SSHSink) -> Result<()> {
-        if !self.sftp_num().is_response() {
-            return Err(Error::bug())
-        }
+//         // packet type
+//         self.sftp_num().enc(s)?;
+//         // request ID
+//         id.0.enc(s)?;
+//         // contents
+//         self.enc(s)
+//     }
+// }
 
-        // packet type
-        self.sftp_num().enc(s)?;
-        // request ID
-        id.0.enc(s)?;
-        // contents
-        self.enc(s)
-    }
-}
+// $(
+// impl<'a> From<$SpecificPacketType> for SftpPacket<'a> {
+//     fn from(s: $SpecificPacketType) -> SftpPacket<'a> {
+//         SftpPacket::$SpecificPacketVariant(s)
+//     }
+// }
+// )*
 
-$(
-impl<'a> From<$SpecificPacketType> for SftpPacket<'a> {
-    fn from(s: $SpecificPacketType) -> SftpPacket<'a> {
-        SftpPacket::$SpecificPacketVariant(s)
-    }
-}
-)*
+// } } // macro
 
-} } // macro
+// sftpmessages![
 
-sftpmessages![
+// // Message number ranges are also used by Sftpnum::is_request and is_response.
 
-// Message number ranges are also used by Sftpnum::is_request and is_response.
+// (1, Init, InitVersion, SSH_FXP_INIT),
+// (2, Version, InitVersion, SSH_FXP_VERSION),
 
-(1, Init, InitVersion, SSH_FXP_INIT),
-(2, Version, InitVersion, SSH_FXP_VERSION),
+// // Requests
+// (3, Open, Open<'a>, SSH_FXP_OPEN),
+// (4, Close, Close<'a>, SSH_FXP_CLOSE),
+// (5, Read, Read<'a>, SSH_FXP_READ),
 
-// Requests
-(3, Open, Open<'a>, SSH_FXP_OPEN),
-(4, Close, Close<'a>, SSH_FXP_CLOSE),
-(5, Read, Read<'a>, SSH_FXP_READ),
+// // Responses
+// (101, Status, Status<'a>, SSH_FXP_STATUS),
+// (102, Handle, Handle<'a>, SSH_FXP_HANDLE),
+// (103, Data, Data<'a>, SSH_FXP_DATA),
+// (104, Name, Name<'a>, SSH_FXP_NAME),
 
-// Responses
-(101, Status, Status<'a>, SSH_FXP_STATUS),
-(102, Handle, Handle<'a>, SSH_FXP_HANDLE),
-(103, Data, Data<'a>, SSH_FXP_DATA),
-(104, Name, Name<'a>, SSH_FXP_NAME),
-
-];
+// ];
