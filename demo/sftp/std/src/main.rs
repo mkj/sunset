6192fb2ca95ea347b356968ac28a681e26045326
jubeormk1 <jubeormk1@gmail.com>
2025-08-30 12:29:56 +1000

    Starting a new sftp-std demo taking the basic std demo as model
    It has some very basic code proving that when an sftp connection is accepted the version handshake starts SSH_FXP_INIT ->
    <- SSH_FXP_VERSION
    etc

---
+++ demo/sftp/std/src/main.rs
@@ -0,0 +1,192 @@
+use embedded_io_async::{Read, Write};
+#[allow(unused_imports)]
+use log::{debug, error, info, log, trace, warn};
+
+use embassy_executor::Spawner;
+use embassy_net::{Stack, StackResources, StaticConfigV4};
+
+use rand::rngs::OsRng;
+use rand::RngCore;
+
+use embassy_futures::select::select;
+use embassy_net_tuntap::TunTapDevice;
+use embassy_sync::channel::Channel;
+
+use sunset::*;
+use sunset_async::{ProgressHolder, SSHServer, SunsetMutex, SunsetRawMutex};
+
+pub(crate) use sunset_demo_common as demo_common;
+
+use demo_common::{DemoCommon, DemoServer, SSHConfig};
+
+const NUM_LISTENERS: usize = 4;
+// +1 for dhcp
+const NUM_SOCKETS: usize = NUM_LISTENERS + 1;
+
+#[embassy_executor::task]
+async fn net_task(mut runner: embassy_net::Runner<'static, TunTapDevice>) -> ! {
+    runner.run().await
+}
+
+#[embassy_executor::task]
+async fn main_task(spawner: Spawner) {
+    // TODO config
+    let opt_tap0 = "tap0";
+    let ip4 = "192.168.69.2";
+    let cir = 24;
+
+    let config = Box::leak(Box::new({
+        let mut config = SSHConfig::new().unwrap();
+        config.set_admin_pw(Some("pw")).unwrap();
+        config.console_noauth = true;
+        config.ip4_static = if let Ok(ip) = ip4.parse() {
+            Some(StaticConfigV4 {
+                address: embassy_net::Ipv4Cidr::new(ip, cir),
+                gateway: None,
+                dns_servers: { heapless::Vec::new() },
+            })
+        } else {
+            None
+        };
+        SunsetMutex::new(config)
+    }));
+
+    let net_cf = if let Some(ref s) = config.lock().await.ip4_static {
+        embassy_net::Config::ipv4_static(s.clone())
+    } else {
+        embassy_net::Config::dhcpv4(Default::default())
+    };
+    info!("Net config: {net_cf:?}");
+
+    // Init network device
+    let net_device = TunTapDevice::new(opt_tap0).unwrap();
+
+    let seed = OsRng.next_u64();
+
+    // Init network stack
+    let res = Box::leak(Box::new(StackResources::<NUM_SOCKETS>::new()));
+    let (stack, runner) = embassy_net::new(net_device, net_cf, res, seed);
+
+    // Launch network task
+    spawner.spawn(net_task(runner)).unwrap();
+
+    for _ in 0..NUM_LISTENERS {
+        spawner.spawn(listen(stack, config)).unwrap();
+    }
+}
+
+#[derive(Default)]
+struct StdDemo;
+
+impl DemoServer for StdDemo {
+    async fn run(&self, serv: &SSHServer<'_>, mut common: DemoCommon) -> Result<()> {
+        let chan_pipe = Channel::<SunsetRawMutex, ChanHandle, 1>::new();
+
+        let prog_loop = async {
+            loop {
+                let mut ph = ProgressHolder::new();
+                let ev = serv.progress(&mut ph).await?;
+                trace!("ev {ev:?}");
+                match ev {
+                    ServEvent::SessionShell(a) => {
+                        a.fail()?; // Not allowed in this example, kept here for compatibility
+                    }
+                    ServEvent::SessionExec(a) => {
+                        a.fail()?; // Not allowed in this example, kept here for compatibility
+                    }
+                    ServEvent::SessionSubsystem(a) => {
+                        match a.command()?.to_lowercase().as_str() {
+                            "sftp" => {
+                                info!("Starting '{}' subsystem", a.command()?);
+
+                                if let Some(ch) = common.sess.take() {
+                                    debug_assert!(ch.num() == a.channel());
+                                    a.succeed()?;
+                                    let _ = chan_pipe.try_send(ch);
+                                } else {
+                                    a.fail()?;
+                                }
+                            }
+                            _ => {
+                                warn!(
+                                "request for subsystem '{}' not implemented: fail",
+                                a.command()?
+                            );
+                                a.fail()?;
+                            }
+                        }
+                    }
+                    other => common.handle_event(other)?,
+                };
+            }
+            #[allow(unreachable_code)]
+            Ok::<_, Error>(())
+        };
+
+        let prog_loop = async {
+            if let Err(e) = prog_loop.await {
+                warn!("Exited: {e:?}");
+            }
+        };
+
+        let sftp_loop = async {
+            let ch = chan_pipe.receive().await;
+            info!("This is the sftp loop");
+            // After this you are likely to receive an SSH_FXP_INIT
+            let mut stdio = serv.stdio(ch).await?;
+
+            loop {
+                let mut b = [0u8; 200];
+                let lr = stdio.read(&mut b).await?;
+                if lr == 0 {
+                    break;
+                }
+                let b = &mut b[..lr];
+                info!("received '{:?}'", b);
+
+                // First packet received! [0, 0, 0, 5, 1, 0, 0, 0, 3], meaning:
+                // Len (u32): [0, 0, 0, 5] 5 bytes
+                // Type (u8): [1] SSH_FXP_INIT
+                // Version (u32): [0, 0, 0, 3]] version 3
+                // So we need to answer 5 SSH_FXP_VERSION 3
+                // [0, 0, 0, 5, 2, 0, 0, 0, 3]
+                let hardcoded_version_r: Vec<u8> = vec![0, 0, 0, 5, 2, 0, 0, 0, 3];
+                info!("sending '{:?}'", hardcoded_version_r);
+
+                stdio.write(hardcoded_version_r.as_slice()).await?;
+            }
+
+            Ok::<_, Error>(())
+        };
+
+        select(prog_loop, sftp_loop).await;
+        todo!()
+    }
+}
+
+// TODO: pool_size should be NUM_LISTENERS but needs a literal
+#[embassy_executor::task(pool_size = 4)]
+async fn listen(
+    stack: Stack<'static>,
+    config: &'static SunsetMutex<SSHConfig>,
+) -> ! {
+    let demo = StdDemo::default();
+    demo_common::listen(stack, config, &demo).await
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Trace)
+        // .filter_module("sunset::runner", log::LevelFilter::Info)
+        .filter_module("sunset::traffic", log::LevelFilter::Info)
+        .filter_module("sunset::encrypt", log::LevelFilter::Info)
+        // .filter_module("sunset::conn", log::LevelFilter::Info)
+        // .filter_module("sunset_async::async_sunset", log::LevelFilter::Info)
+        .filter_module("async_io", log::LevelFilter::Info)
+        .filter_module("polling", log::LevelFilter::Info)
+        .format_timestamp_nanos()
+        .init();
+
+    spawner.spawn(main_task(spawner)).unwrap();
+}
