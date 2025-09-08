ff41e437d14d436a66ac30adb77bffe1ac676212
jubeormk1 <jubeormk1@gmail.com>
2025-09-09 05:45:13 +1000

    Reformed sftpserver
    - flags are no longer required
    - removed ItemHandle
    - Added Into and TryFrom File Handle for SftpServer trait
    - Added lifetime 'a to Handle and the trait associated function taking or returning the type
    
    minor changes in proto:
    added clone, copy and compare implements to FileHandle and TextString

--- sftp/src/sftpserver.rs
+++ sftp/src/sftpserver.rs
@@ -1,41 +1,48 @@
-use crate::proto::{Attrs, Name, StatusCode};
+use crate::proto::{Attrs, FileHandle, Name, StatusCode};
 
+use core::fmt::Debug;
 use core::marker::PhantomData;
 
-pub type SftpResult<T> = core::result::Result<T, StatusCode>;
+pub type SftpOpResult<T> = core::result::Result<T, StatusCode>;
 
 /// All trait functions are optional in the SFTP protocol.
 /// Some less core operations have a Provided implementation returning
 /// returns `SSH_FX_OP_UNSUPPORTED`. Common operations must be implemented,
 /// but may return `Err(StatusCode::SSH_FX_OP_UNSUPPORTED)`.
 pub trait SftpServer {
-    type Handle;
+    // type Handle: Into<FileHandle> + TryFrom<FileHandle> + Debug;
+    type Handle<'a>: Into<FileHandle<'a>> + TryFrom<FileHandle<'a>> + Debug + Copy;
 
     // TODO flags struct
-    async fn open(
+    async fn open<'a>(
         filename: &str,
-        flags: u32,
         attrs: &Attrs,
-    ) -> SftpResult<Self::Handle>;
+    ) -> SftpOpResult<Self::Handle<'a>>;
 
     /// Close either a file or directory handle
-    async fn close(handle: &Self::Handle) -> SftpResult<()>;
+    async fn close<'a>(handle: &Self::Handle<'a>) -> SftpOpResult<()>;
 
-    async fn read(
-        handle: &Self::Handle,
+    async fn read<'a>(
+        handle: &Self::Handle<'a>,
         offset: u64,
         reply: &mut ReadReply,
-    ) -> SftpResult<()>;
+    ) -> SftpOpResult<()>;
 
-    async fn write(handle: &Self::Handle, offset: u64, buf: &[u8])
-    -> SftpResult<()>;
+    async fn write<'a>(
+        handle: &Self::Handle<'a>,
+        offset: u64,
+        buf: &[u8],
+    ) -> SftpOpResult<()>;
 
-    async fn opendir(dir: &str) -> SftpResult<Self::Handle>;
+    async fn opendir<'a>(dir: &str) -> SftpOpResult<Self::Handle<'a>>;
 
-    async fn readdir(handle: &Self::Handle, reply: &mut DirReply) -> SftpResult<()>;
+    async fn readdir<'a>(
+        handle: &Self::Handle<'a>,
+        reply: &mut DirReply,
+    ) -> SftpOpResult<()>;
 
     /// Provides the real path of the directory specified
-    async fn realpath(dir: &str) -> SftpResult<Name<'_>>;
+    async fn realpath(dir: &str) -> SftpOpResult<Name<'_>>;
 }
 
 pub struct ReadReply<'g, 'a> {
@@ -58,9 +65,4 @@ impl<'g, 'a> DirReply<'g, 'a> {
 pub struct ChanOut<'g, 'a> {
     _phantom_g: PhantomData<&'g ()>,
     _phantom_a: PhantomData<&'a ()>,
-}
-
-#[derive(Debug)]
-pub struct ItemHandle {
-    client_opaque_handle: String,
 }
