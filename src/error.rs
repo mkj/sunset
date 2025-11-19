mkj/sunset

commit 7bea15b0251dfca8ab95f5884e0b4153a9510b82

Give Kex a CliServ generic parameter
This is a small code size saving (less than I'd hoped), but on balance
it also removes some "TODO client vs server" and makes them more
infallible.

src/runner.rs
@@ -84,6 +84,113 @@ impl<'a> Runner<'a, client::Client> {
     ) -> Runner<'a, client::Client> {
         Self::new(inbuf, outbuf)
     }
+
+    /// Send a break to a session channel
+    ///
+    /// `length` is in milliseconds, or
+    /// pass 0 as a default (to be interpreted by the remote implementation).
+    /// Otherwise length will be clamped to the range [500, 3000] ms.
+    /// Only call on a client session.
+    pub fn term_break(&mut self, chan: &ChanHandle, length: u32) -> Result<()> {
+        let mut s = self.traf_out.sender(&mut self.keys);
+        self.conn.channels.term_break(chan.0, length, &mut s)
+    }
+
+    pub(crate) fn fetch_cli_session_exit(&mut self) -> Result<CliSessionExit<'_>> {
+        let (payload, _seq) = self.traf_in.payload().trap()?;
+        self.conn.fetch_cli_session_exit(payload)
+    }
+
+    pub(crate) fn fetch_cli_banner(&mut self) -> Result<event::Banner<'_>> {
+        let (payload, _seq) = self.traf_in.payload().trap()?;
+        self.conn.fetch_cli_banner(payload)
+    }
+
+    pub(crate) fn cli_session_opener(
+        &mut self,
+        ch: ChanNum,
+    ) -> Result<CliSessionOpener<'_, 'a>> {
+        let ch = self.conn.channels.get(ch)?;
+        let s = self.traf_out.sender(&mut self.keys);
+
+        Ok(CliSessionOpener { ch, s })
+    }
+
+    pub(crate) fn resume_cliusername(&mut self, username: &str) -> Result<()> {
+        self.resume(&DispatchEvent::CliEvent(CliEventId::Username));
+        let mut s = self.traf_out.sender(&mut self.keys);
+        let (cliauth, _) = self.conn.mut_cliauth()?;
+        cliauth.resume_username(&mut s, username)?;
+        self.traf_in.done_payload();
+        Ok(())
+    }
+
+    pub(crate) fn resume_clipassword(
+        &mut self,
+        password: Option<&str>,
+    ) -> Result<()> {
+        self.resume(&DispatchEvent::CliEvent(CliEventId::Password));
+        self.traf_in.done_payload();
+        let mut s = self.traf_out.sender(&mut self.keys);
+        let (cliauth, ctx) = self.conn.mut_cliauth()?;
+        cliauth.resume_password(&mut s, password, ctx)?;
+        // assert that resume_password() returns error with none password.
+        // otherwise we might need to handle other events like with clipubkey
+        debug_assert!(password.is_some(), "no password");
+        Ok(())
+    }
+
+    pub(crate) fn resume_clipubkey(&mut self, key: Option<SignKey>) -> Result<()> {
+        self.resume(&DispatchEvent::CliEvent(CliEventId::Pubkey));
+        let mut s = self.traf_out.sender(&mut self.keys);
+        let (cliauth, ctx) = self.conn.mut_cliauth()?;
+        self.extra_resume_event = cliauth.resume_pubkey(&mut s, key, ctx)?;
+        if self.extra_resume_event.is_none() {
+            self.traf_in.done_payload();
+        }
+        Ok(())
+    }
+
+    pub(crate) fn fetch_agentsign_key(&self) -> Result<&SignKey> {
+        self.check_resume(&DispatchEvent::CliEvent(CliEventId::AgentSign));
+        let cliauth = self.conn.cliauth()?;
+        cliauth.fetch_agentsign_key()
+    }
+
+    pub(crate) fn fetch_agentsign_msg(&self) -> Result<AuthSigMsg<'_>> {
+        self.check_resume(&DispatchEvent::CliEvent(CliEventId::AgentSign));
+        self.conn.fetch_agentsign_msg()
+    }
+
+    pub(crate) fn resume_agentsign(&mut self, sig: Option<&OwnedSig>) -> Result<()> {
+        self.resume(&DispatchEvent::CliEvent(CliEventId::AgentSign));
+        let (cliauth, ctx) = self.conn.mut_cliauth()?;
+        let mut s = self.traf_out.sender(&mut self.keys);
+        self.extra_resume_event = cliauth.resume_agentsign(sig, ctx, &mut s)?;
+        if self.extra_resume_event.is_none() {
+            self.traf_in.done_payload();
+        }
+        Ok(())
+    }
+
+    pub(crate) fn resume_checkhostkey(&mut self, accept: bool) -> Result<()> {
+        self.resume(&DispatchEvent::CliEvent(CliEventId::Hostkey));
+
+        let (payload, _seq) = self.traf_in.payload().trap()?;
+        let mut s = self.traf_out.sender(&mut self.keys);
+
+        self.conn.resume_checkhostkey(payload, &mut s, accept)?;
+        self.traf_in.done_payload();
+        Ok(())
+    }
+
+    pub(crate) fn fetch_checkhostkey(&self) -> Result<PubKey<'_>> {
+        self.check_resume(&DispatchEvent::CliEvent(CliEventId::Hostkey));
+
+        let (payload, _seq) = self.traf_in.payload().trap()?;
+
+        self.conn.fetch_checkhostkey(payload)
+    }
 }
 
 impl<'a> Runner<'a, server::Server> {
@@ -93,6 +200,67 @@ impl<'a> Runner<'a, server::Server> {
         outbuf: &'a mut [u8],
     ) -> Runner<'a, server::Server> {
         Self::new(inbuf, outbuf)
+    }
+
+    pub(crate) fn resume_servhostkeys(&mut self, keys: &[&SignKey]) -> Result<()> {
+        self.resume(&DispatchEvent::ServEvent(ServEventId::Hostkeys));
+        let (payload, _seq) = self.traf_in.payload().trap()?;
+        let mut s = self.traf_out.sender(&mut self.keys);
+        self.conn.resume_servhostkeys(payload, &mut s, keys)?;
+        self.traf_in.done_payload();
+        Ok(())
+    }
+
+    pub(crate) fn fetch_servusername(&self) -> Result<TextString<'_>> {
+        let u = self.conn.server()?.auth.username.as_ref().trap()?;
+        Ok(TextString(u.as_slice()))
+    }
+
+    pub(crate) fn fetch_servpassword(&self) -> Result<TextString<'_>> {
+        self.check_resume(&DispatchEvent::ServEvent(ServEventId::PasswordAuth));
+        let (payload, _seq) = self.traf_in.payload().trap()?;
+        self.conn.fetch_servpassword(payload)
+    }
+
+    pub(crate) fn fetch_servpubkey(&self) -> Result<PubKey<'_>> {
+        self.check_resume(&DispatchEvent::ServEvent(ServEventId::PubkeyAuth {
+            real_sig: false,
+        }));
+        let (payload, _seq) = self.traf_in.payload().trap()?;
+        self.conn.fetch_servpubkey(payload)
+    }
+
+    pub(crate) fn resume_servauth(&mut self, allow: bool) -> Result<()> {
+        let prev_event = self.resume_event.take();
+        // auth packets have passwords
+        self.traf_in.zeroize_payload();
+        debug_assert!(
+            matches!(
+                prev_event,
+                DispatchEvent::ServEvent(ServEventId::PasswordAuth)
+            ) || matches!(
+                prev_event,
+                DispatchEvent::ServEvent(ServEventId::PubkeyAuth { .. })
+            ) || matches!(
+                prev_event,
+                DispatchEvent::ServEvent(ServEventId::FirstAuth)
+            )
+        );
+
+        let mut s = self.traf_out.sender(&mut self.keys);
+        self.conn.resume_servauth(allow, &mut s)
+    }
+
+    pub(crate) fn resume_servauth_pkok(&mut self) -> Result<()> {
+        self.resume(&DispatchEvent::ServEvent(ServEventId::PubkeyAuth {
+            real_sig: false,
+        }));
+
+        let (payload, _seq) = self.traf_in.payload().trap()?;
+        let mut s = self.traf_out.sender(&mut self.keys);
+        let r = self.conn.resume_servauth_pkok(payload, &mut s);
+        self.traf_in.done_payload();
+        r
     }
 }
 
@@ -519,43 +687,9 @@ impl<'a, CS: CliServ> Runner<'a, CS> {
             let mut s = self.traf_out.sender(&mut self.keys);
             self.conn.channels.term_window_change(chan.0, winch, &mut s)
         } else {
-            error::BadChannelData.fail()
+            trace!("winch as server");
+            Err(Error::BadUsage {})
         }
-    }
-
-    /// Send a break to a session channel
-    ///
-    /// `length` is in milliseconds, or
-    /// pass 0 as a default (to be interpreted by the remote implementation).
-    /// Otherwise length will be clamped to the range [500, 3000] ms.
-    /// Only call on a client session.
-    pub fn term_break(&mut self, chan: &ChanHandle, length: u32) -> Result<()> {
-        if CS::is_client() {
-            let mut s = self.traf_out.sender(&mut self.keys);
-            self.conn.channels.term_break(chan.0, length, &mut s)
-        } else {
-            error::BadChannelData.fail()
-        }
-    }
-
-    pub(crate) fn cli_session_opener(
-        &mut self,
-        ch: ChanNum,
-    ) -> Result<CliSessionOpener<'_, 'a>> {
-        let ch = self.conn.channels.get(ch)?;
-        let s = self.traf_out.sender(&mut self.keys);
-
-        Ok(CliSessionOpener { ch, s })
-    }
-
-    pub(crate) fn fetch_cli_session_exit(&mut self) -> Result<CliSessionExit<'_>> {
-        let (payload, _seq) = self.traf_in.payload().trap()?;
-        self.conn.fetch_cli_session_exit(payload)
-    }
-
-    pub(crate) fn fetch_cli_banner(&mut self) -> Result<event::Banner<'_>> {
-        let (payload, _seq) = self.traf_in.payload().trap()?;
-        self.conn.fetch_cli_banner(payload)
     }
 
     // Wake SSH TCP socket input and output as required.
@@ -611,143 +745,6 @@ impl<'a, CS: CliServ> Runner<'a, CS> {
 
     fn check_resume(&self, expect: &DispatchEvent) {
         self.check_resume_inner(expect, &self.resume_event)
-    }
-
-    pub(crate) fn resume_cliusername(&mut self, username: &str) -> Result<()> {
-        self.resume(&DispatchEvent::CliEvent(CliEventId::Username));
-        let mut s = self.traf_out.sender(&mut self.keys);
-        let (cliauth, _) = self.conn.mut_cliauth()?;
-        cliauth.resume_username(&mut s, username)?;
-        self.traf_in.done_payload();
-        Ok(())
-    }
-
-    pub(crate) fn resume_clipassword(
-        &mut self,
-        password: Option<&str>,
-    ) -> Result<()> {
-        self.resume(&DispatchEvent::CliEvent(CliEventId::Password));
-        self.traf_in.done_payload();
-        let mut s = self.traf_out.sender(&mut self.keys);
-        let (cliauth, ctx) = self.conn.mut_cliauth()?;
-        cliauth.resume_password(&mut s, password, ctx)?;
-        // assert that resume_password() returns error with none password.
-        // otherwise we might need to handle other events like with clipubkey
-        debug_assert!(password.is_some(), "no password");
-        Ok(())
-    }
-
-    pub(crate) fn resume_clipubkey(&mut self, key: Option<SignKey>) -> Result<()> {
-        self.resume(&DispatchEvent::CliEvent(CliEventId::Pubkey));
-        let mut s = self.traf_out.sender(&mut self.keys);
-        let (cliauth, ctx) = self.conn.mut_cliauth()?;
-        self.extra_resume_event = cliauth.resume_pubkey(&mut s, key, ctx)?;
-        if self.extra_resume_event.is_none() {
-            self.traf_in.done_payload();
-        }
-        Ok(())
-    }
-
-    pub(crate) fn fetch_agentsign_key(&self) -> Result<&SignKey> {
-        self.check_resume(&DispatchEvent::CliEvent(CliEventId::AgentSign));
-        let cliauth = self.conn.cliauth()?;
-        cliauth.fetch_agentsign_key()
-    }
-
-    pub(crate) fn fetch_agentsign_msg(&self) -> Result<AuthSigMsg<'_>> {
-        self.check_resume(&DispatchEvent::CliEvent(CliEventId::AgentSign));
-        self.conn.fetch_agentsign_msg()
-    }
-
-    pub(crate) fn resume_agentsign(&mut self, sig: Option<&OwnedSig>) -> Result<()> {
-        self.resume(&DispatchEvent::CliEvent(CliEventId::AgentSign));
-        let (cliauth, ctx) = self.conn.mut_cliauth()?;
-        let mut s = self.traf_out.sender(&mut self.keys);
-        self.extra_resume_event = cliauth.resume_agentsign(sig, ctx, &mut s)?;
-        if self.extra_resume_event.is_none() {
-            self.traf_in.done_payload();
-        }
-        Ok(())
-    }
-
-    pub(crate) fn resume_checkhostkey(&mut self, accept: bool) -> Result<()> {
-        self.resume(&DispatchEvent::CliEvent(CliEventId::Hostkey));
-
-        let (payload, _seq) = self.traf_in.payload().trap()?;
-        let mut s = self.traf_out.sender(&mut self.keys);
-
-        self.conn.resume_checkhostkey(payload, &mut s, accept)?;
-        self.traf_in.done_payload();
-        Ok(())
-    }
-
-    pub(crate) fn fetch_checkhostkey(&self) -> Result<PubKey<'_>> {
-        self.check_resume(&DispatchEvent::CliEvent(CliEventId::Hostkey));
-
-        let (payload, _seq) = self.traf_in.payload().trap()?;
-
-        self.conn.fetch_checkhostkey(payload)
-    }
-
-    pub(crate) fn resume_servhostkeys(&mut self, keys: &[&SignKey]) -> Result<()> {
-        self.resume(&DispatchEvent::ServEvent(ServEventId::Hostkeys));
-        let (payload, _seq) = self.traf_in.payload().trap()?;
-        let mut s = self.traf_out.sender(&mut self.keys);
-        self.conn.resume_servhostkeys(payload, &mut s, keys)?;
-        self.traf_in.done_payload();
-        Ok(())
-    }
-
-    pub(crate) fn fetch_servusername(&self) -> Result<TextString<'_>> {
-        let u = self.conn.server()?.auth.username.as_ref().trap()?;
-        Ok(TextString(u.as_slice()))
-    }
-
-    pub(crate) fn fetch_servpassword(&self) -> Result<TextString<'_>> {
-        self.check_resume(&DispatchEvent::ServEvent(ServEventId::PasswordAuth));
-        let (payload, _seq) = self.traf_in.payload().trap()?;
-        self.conn.fetch_servpassword(payload)
-    }
-
-    pub(crate) fn fetch_servpubkey(&self) -> Result<PubKey<'_>> {
-        self.check_resume(&DispatchEvent::ServEvent(ServEventId::PubkeyAuth {
-            real_sig: false,
-        }));
-        let (payload, _seq) = self.traf_in.payload().trap()?;
-        self.conn.fetch_servpubkey(payload)
-    }
-
-    pub(crate) fn resume_servauth(&mut self, allow: bool) -> Result<()> {
-        let prev_event = self.resume_event.take();
-        // auth packets have passwords
-        self.traf_in.zeroize_payload();
-        debug_assert!(
-            matches!(
-                prev_event,
-                DispatchEvent::ServEvent(ServEventId::PasswordAuth)
-            ) || matches!(
-                prev_event,
-                DispatchEvent::ServEvent(ServEventId::PubkeyAuth { .. })
-            ) || matches!(
-                prev_event,
-                DispatchEvent::ServEvent(ServEventId::FirstAuth)
-            )
-        );
-
-        let mut s = self.traf_out.sender(&mut self.keys);
-        self.conn.resume_servauth(allow, &mut s)
-    }
-
-    pub(crate) fn resume_servauth_pkok(&mut self) -> Result<()> {
-        self.resume(&DispatchEvent::ServEvent(ServEventId::PubkeyAuth {
-            real_sig: false,
-        }));
-
-        let (payload, _seq) = self.traf_in.payload().trap()?;
-        let mut s = self.traf_out.sender(&mut self.keys);
-        let r = self.conn.resume_servauth_pkok(payload, &mut s);
-        self.traf_in.done_payload();
-        r
     }
 
     pub(crate) fn resume_chanopen(

async/src/async_sunset.rs
@@ -478,6 +478,7 @@ pub(crate) trait ChanCore: MaybeSend {
         buf: &[u8],
     ) -> Poll<Result<usize>>;
 
+    // Client only
     fn poll_term_window_change(
         &self,
         cx: &mut Context,

src/cliauth.rs
@@ -26,6 +26,7 @@ use sshnames::*;
 use sshwire::{BinString, Blob};
 use traffic::TrafSend;
 
+#[derive(Debug)]
 enum AuthState {
     Unstarted,
     MethodQuery,
@@ -35,6 +36,7 @@ enum AuthState {
     Idle,
 }
 
+#[derive(Debug)]
 pub(crate) struct CliAuth {
     state: AuthState,
 

src/client.rs
@@ -14,7 +14,7 @@ use sign::SignKey;
 use sshnames::*;
 use traffic::TrafSend;
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct Client {
     pub(crate) auth: CliAuth,
 }

src/conn.rs
@@ -34,7 +34,7 @@ pub(crate) struct Conn<CS: CliServ> {
     state: ConnState,
 
     // State of any current Key Exchange
-    kex: Kex,
+    kex: Kex<CS>,
 
     sess_id: Option<SessId>,
 
@@ -438,7 +438,6 @@ impl<CS: CliServ> Conn<CS> {
             Packet::KexInit(k) => {
                 self.kex.handle_kexinit(
                     k,
-                    self.is_client(),
                     &self.algo_conf,
                     &self.remote_version,
                     self.is_first_kex(),
@@ -446,22 +445,10 @@ impl<CS: CliServ> Conn<CS> {
                 )?;
             }
             Packet::KexDHInit(_p) => {
-                if self.is_client() {
-                    // TODO: client/server validity checks should move somewhere more general
-                    trace!("kexdhinit not server");
-                    return error::SSHProto.fail();
-                }
-
                 disp.event = self.kex.handle_kexdhinit()?;
             }
             Packet::KexDHReply(_p) => {
-                if !self.is_client() {
-                    // TODO: client/server validity checks should move somewhere more general
-                    trace!("kexdhreply not server");
-                    return error::SSHProto.fail();
-                }
-
-                disp.event = self.kex.handle_kexdhreply();
+                disp.event = self.kex.handle_kexdhreply()?;
             }
             Packet::NewKeys(_) => {
                 self.kex.handle_newkeys(&mut self.sess_id, s)?;
@@ -572,7 +559,9 @@ impl<CS: CliServ> Conn<CS> {
         };
         Ok(disp)
     }
+}
 
+impl Conn<Client> {
     pub(crate) fn cliauth(&self) -> Result<&CliAuth> {
         let cli = self.client()?;
         Ok(&cli.auth)
@@ -607,7 +596,7 @@ impl<CS: CliServ> Conn<CS> {
                 return error::BadUsage.fail();
             }
 
-            self.kex.resume_kexdhreply(&p, self.is_first_kex(), s)
+            self.kex.resume_kexdhreply(&p, s)
         } else {
             Err(Error::bug())
         }
@@ -647,7 +636,9 @@ impl<CS: CliServ> Conn<CS> {
             Err(Error::bug())
         }
     }
+}
 
+impl Conn<Server> {
     pub(crate) fn resume_servhostkeys(
         &mut self,
         payload: &[u8],
@@ -658,7 +649,7 @@ impl<CS: CliServ> Conn<CS> {
 
         let packet = self.packet(payload)?;
         if let Packet::KexDHInit(p) = packet {
-            self.kex.resume_kexdhinit(&p, keys, s)
+            self.kex.resume_kexdhinit(&p, self.is_first_kex(), keys, s)
         } else {
             Err(Error::bug())
         }
@@ -681,7 +672,6 @@ impl<CS: CliServ> Conn<CS> {
             Err(Error::bug())
         }
     }
-
     pub(crate) fn fetch_servpubkey<'f>(
         &self,
         payload: &'f [u8],

src/encrypt.rs
@@ -179,19 +179,21 @@ impl Keys {
         }
     }
 
-    pub fn derive(
+    pub fn new<CS: CliServ>(
         kex_out: kex::KexOutput,
         sess_id: &SessId,
-        algos: &kex::Algos,
-    ) -> Result<Self, Error> {
+        algos: &kex::Algos<CS>,
+    ) -> Self {
         let mut key = [0u8; MAX_KEY_LEN];
         let mut iv = [0u8; MAX_IV_LEN];
 
-        let [iv_e, iv_d, k_e, k_d, i_e, i_d] = if algos.is_client {
+        let [iv_e, iv_d, k_e, k_d, i_e, i_d] = if CS::is_client() {
             ['A', 'B', 'C', 'D', 'E', 'F']
         } else {
             ['B', 'A', 'D', 'C', 'F', 'E']
         };
+
+        // OK to unwrap here, have tests for all iv_len and key_len
 
         let enc = {
             let ci = kex_out.compute_key(
@@ -199,14 +201,14 @@ impl Keys {
                 algos.cipher_enc.iv_len(),
                 &mut iv,
                 sess_id,
-            )?;
+            );
             let ck = kex_out.compute_key(
                 k_e,
                 algos.cipher_enc.key_len(),
                 &mut key,
                 sess_id,
-            )?;
-            EncKey::from_cipher(&algos.cipher_enc, ck, ci)?
+            );
+            EncKey::from_cipher(&algos.cipher_enc, ck, ci).unwrap()
         };
 
         let dec = {
@@ -215,14 +217,14 @@ impl Keys {
                 algos.cipher_dec.iv_len(),
                 &mut iv,
                 sess_id,
-            )?;
+            );
             let ck = kex_out.compute_key(
                 k_d,
                 algos.cipher_dec.key_len(),
                 &mut key,
                 sess_id,
-            )?;
-            DecKey::from_cipher(&algos.cipher_dec, ck, ci)?
+            );
+            DecKey::from_cipher(&algos.cipher_dec, ck, ci).unwrap()
         };
 
         let integ_enc = {
@@ -231,8 +233,8 @@ impl Keys {
                 algos.integ_enc.key_len(),
                 &mut key,
                 sess_id,
-            )?;
-            IntegKey::from_integ(&algos.integ_enc, ck)?
+            );
+            IntegKey::from_integ(&algos.integ_enc, ck).unwrap()
         };
 
         let integ_dec = {
@@ -241,11 +243,11 @@ impl Keys {
                 algos.integ_dec.key_len(),
                 &mut key,
                 sess_id,
-            )?;
-            IntegKey::from_integ(&algos.integ_dec, ck)?
+            );
+            IntegKey::from_integ(&algos.integ_dec, ck).unwrap()
         };
 
-        Ok(Keys { enc, dec, integ_enc, integ_dec })
+        Keys { enc, dec, integ_enc, integ_dec }
     }
 
     /// Decrypts the first block in the buffer
@@ -676,6 +678,8 @@ impl IntegKey {
 
 #[cfg(test)]
 mod tests {
+    use core::marker::PhantomData;
+
     use crate::encrypt::*;
     use crate::error::Error;
     use crate::kex::KexOutput;
@@ -739,7 +743,7 @@ mod tests {
 
     // returns combinations of ciphers as Some(), as well as a single
     // None for no-cipher
-    fn algo_combos() -> impl Iterator<Item = Option<kex::Algos>> {
+    fn algo_combos() -> impl Iterator<Item = Option<kex::Algos<Client>>> {
         // TODO make this combinatorial
         // order is enc, dec
         const COMBOS: [(Cipher, Integ, Cipher, Integ); 4] = [
@@ -764,9 +768,9 @@ mod tests {
                     integ_enc: ie.clone(),
                     integ_dec: id.clone(),
                     discard_next: false,
-                    is_client: false,
                     send_ext_info: true,
                     strict_kex: false,
+                    _cs: PhantomData,
                 })
             })
             // and plaintext
@@ -780,7 +784,7 @@ mod tests {
         for mut algos in algo_combos() {
             let mut keys_enc = KeyState::new_cleartext();
             let mut keys_dec = KeyState::new_cleartext();
-            if let Some(ref mut algos) = algos {
+            if let Some(algos) = algos.take() {
                 // arbitrary keys
                 let h = SessId::from_slice(&Sha256::digest(
                     "some exchange hash".as_bytes(),
@@ -794,16 +798,14 @@ mod tests {
                 let ko_b = KexOutput::new_test(sharedkey, &h);
 
                 trace!("algos enc {algos:?}");
-                let newkeys = Keys::derive(ko, &sess_id, &algos).unwrap();
+                let newkeys = Keys::new(ko, &sess_id, &algos);
                 keys_enc.rekey(newkeys);
 
                 // client and server enc/dec keys are derived differently, we need them
                 // to match for this test
-                algos.is_client = !algos.is_client;
-                core::mem::swap(&mut algos.cipher_enc, &mut algos.cipher_dec);
-                core::mem::swap(&mut algos.integ_enc, &mut algos.integ_dec);
+                let algos = algos.test_swap_to_server();
                 trace!("algos dec {algos:?}");
-                let newkeys_b = Keys::derive(ko_b, &sess_id, &algos).unwrap();
+                let newkeys_b = Keys::new(ko_b, &sess_id, &algos);
                 keys_dec.rekey(newkeys_b);
             } else {
                 trace!("Trying cleartext");
@@ -830,7 +832,7 @@ mod tests {
                     SessId::from_slice(&Sha256::digest(b"some sessid")).unwrap();
                 let sharedkey = b"hello";
                 let ko = KexOutput::new_test(sharedkey, &h);
-                let newkeys = Keys::derive(ko, &sess_id, &algos).unwrap();
+                let newkeys = Keys::new(ko, &sess_id, &algos);
 
                 keys.rekey(newkeys);
                 trace!("algos {algos:?}");

src/servauth.rs
@@ -19,6 +19,7 @@ use heapless::{String, Vec};
 /// `methods_` can be during runtime, though if they
 /// are changed after the auth process starts it's unknown
 /// if client implementations will handle varying auth methods correctly.
+#[derive(Debug)]
 pub(crate) struct ServAuth {
     pub authed: bool,
 

commit e09c03cfea9c2e191b82e4a74d5410e8c83739cd

Fix building with backtrace feature
Test it in CI as well.

Fixes: 7bea15b0251d ("Give Kex a CliServ generic parameter")
Fixes: b5e56871b4b5 ("Fix building with backtrace feature")

src/error.rs
@@ -8,6 +8,9 @@ use snafu::prelude::*;
 
 use crate::channel::ChanNum;
 
+#[allow(unused_imports)]
+use snafu::{Backtrace, Location};
+
 // TODO: can we make Snafu not require Debug?
 
 /// The Sunset error type.

src/runner.rs
@@ -693,7 +693,7 @@ impl<'a, CS: CliServ> Runner<'a, CS> {
             self.conn.channels.term_window_change(chan.0, winch, &mut s)
         } else {
             trace!("winch as server");
-            Err(Error::BadUsage {})
+            Err(error::BadUsage.build())
         }
     }
 

testing/ci.sh
@@ -42,7 +42,8 @@ cargo test --doc
 cd stdasync
 # only test lib since some examples are broken
 cargo test --lib
-cargo build --example sunsetc
+# test backtrace feature too
+cargo build --example sunsetc --features sunset/backtrace
 # with/without release to test debug_assertions
 cargo build --release --example sunsetc
 )