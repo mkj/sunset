6792cec7bfc2e02c30b50b7d6e6fb583854803f4
jubeormk1 <jubeormk1@gmail.com>
2025-11-21 09:59:47 +1100

    [skip ci] Adding test_get_single.sh and simplifying test_get.sh
    The upload process has been removed to reduce verbosity and test time. Also keeps the test focus on dowloading items.
    
    test_get_single.sh only performs a get transaction of a long file (4MB)
    
    Tests will keep failing check files for further inspection

--- demo/sftp/std/testing/test_get_single.sh
+++ demo/sftp/std/testing/test_get_single.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Testing Multiple GETs..."
+echo "Testing Single long GETs..."
 
 echo "Cleaning up previous run files"
 rm -f -r ./*_random ./out/*_random
@@ -11,17 +11,17 @@ REMOTE_HOST="192.168.69.2"
 REMOTE_USER="any"
 
 # Define test files
-FILES=("512B_random" "16kB_random" "64kB_random" "65kB_random" "2048kB_random")
-
+# FILES=("512B_random" "16kB_random" "64kB_random" "65kB_random" "2048kB_random")
+FILES=("4096kB_random")
 
 echo "Generating random data files..."
-dd if=/dev/random bs=512 count=1 of=./512B_random 2>/dev/null
-dd if=/dev/random bs=1024 count=16 of=./16kB_random 2>/dev/null
-dd if=/dev/random bs=1024 count=64 of=./64kB_random 2>/dev/null
-dd if=/dev/random bs=1024 count=65 of=./65kB_random 2>/dev/null
-dd if=/dev/random bs=1024 count=256 of=./256kB_random 2>/dev/null
-dd if=/dev/random bs=1024 count=1024 of=./1024kB_random 2>/dev/null
-dd if=/dev/random bs=1024 count=2048 of=./2048kB_random 2>/dev/null
+# dd if=/dev/random bs=512 count=1 of=./512B_random 2>/dev/null
+# dd if=/dev/random bs=1024 count=16 of=./16kB_random 2>/dev/null
+# dd if=/dev/random bs=1024 count=64 of=./64kB_random 2>/dev/null
+# dd if=/dev/random bs=1024 count=65 of=./65kB_random 2>/dev/null
+# dd if=/dev/random bs=1024 count=256 of=./256kB_random 2>/dev/null
+# dd if=/dev/random bs=1024 count=1024 of=./1024kB_random 2>/dev/null
+dd if=/dev/random bs=1024 count=4096 of=./4096kB_random 2>/dev/null
 echo "Uploading files to ${REMOTE_USER}@${REMOTE_HOST}..."
 
 echo "Moving to the server folder..."
