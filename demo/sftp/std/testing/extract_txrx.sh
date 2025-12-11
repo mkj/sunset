1760b63bb73c50a87d7009aa5229e5d004ee73f5
jubeormk1 <jubeormk1@gmail.com>
2025-12-12 10:26:56 +1100

    [skip ci] Extracting sftpoutputproducer bytes for analysis
    At this point I can see that on stall, the backpressure from the ChanOut blocks the data put in the pipe.
    
    I took measures in a previous commit (5d1c1f9424aa4ec5eb00450e4c21e2dde96eaaf9) to guarantee that for SSH_FXP_DATA and SSH_FXP_NAME the announced length and real length matches.
    - SftpServer Unit tests to check the length in header and data sent
    - Runtime checks for ReadDir, Read and PathInfo requests
    
    I am going to start digging in the ChanOut Write process

--- demo/sftp/std/testing/extract_txrx.sh
+++ demo/sftp/std/testing/extract_txrx.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-# Find all lines containing SFTP... OR Output Consumer...
+# Find all lines containing SFTP... OR Output Consumer... OR Output Producer...
+# and reformat them into a simpler form for further processing.
+
+
+cat <<EOF > ${1}.txrx
+Extracting communications from sunset-demo-sftp-std log file: $1
+Extract of RX (c: Client), TX (s: server), And internal TX (p: pipe producer)
+------------------------------------------------
+EOF
+
 cat $1 | \
-grep -E 'SFTP <---- received: \[|Output Consumer: Bytes written \[' | \
-sed 's/.*received: /c / ; s/.*written /s /'  > ${1}.txrx
+grep -E 'SFTP <---- received: \[|Output Consumer: Bytes written \[|Output Producer: Sending buffer \[' | \
+sed 's/.*received: /c / ; s/.*written /s / ; s/.*Output Producer: Sending buffer /p /'  >> ${1}.txrx
+
+
 # Extract received lines. Remove brackets, spaces,
 # and split by comma into new lines. Finally remove empty lines.
 
@@ -14,6 +25,14 @@ sed 's/.*received: //' | \
 sed 's/\[//; s/\]/,/' | \
 tr -d ' ' |tr ',' '\n'| \
 grep -v '^$' > ${1}.rx
+
+# Producer
+cat $1 | \
+grep -E 'Output Producer: Sending buffer \[' | \
+sed 's/.*buffer //' | \
+sed 's/\[//; s/\]/,/' | \
+tr -d ' ' |tr ',' '\n'| \
+grep -v '^$' > ${1}.txp
 
 # TX
 cat $1 | \
