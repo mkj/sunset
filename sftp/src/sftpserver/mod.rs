f68c79edff838ff00d34ba10f3f91578ea2db50e
jubeormk1 <jubeormk1@gmail.com>
2026-04-30 15:05:17 +1000

    WIP: Starting to replace DirReply with Enforcing readdirreplies

--- sftp/src/sftpserver/mod.rs
+++ sftp/src/sftpserver/mod.rs
@@ -7,14 +7,17 @@ pub use sftpserver::SftpServer;
 
 pub use sftpserver::ReadStatus;
 
-pub use sftpserver::DirReply;
-
 pub use sftpserver::SftpOpResult;
-pub use sftpserver::helpers;
+
+pub use crate::sftpserver::readdirreplies::no_std_helpers;
 
 #[cfg(feature = "std")]
-pub use sftpserver::DirEntriesCollection;
-#[cfg(feature = "std")]
-pub use sftpserver::get_file_attrs;
+pub use crate::sftpserver::readdirreplies::std_helpers::{
+    DirEntriesCollection, get_file_attrs,
+};
 
 pub use readreplies::{ReadHeaderReply, ReadReplyFinished};
+
+pub use readdirreplies::{
+    DirReadDataReply, DirReadHeaderReply, DirReadReplyFinished,
+};
