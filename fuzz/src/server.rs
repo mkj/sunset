mkj/sunset

commit 73a8142b4302abbf64d4a586864ffbbe8703c2cb

Remove ServPubkeyAuth::real()
Applications shouldn't have any special handling for test versus actual
pubkey auth requests. Remove the real() function since it can cause
confusion.

fuzz/src/server.rs
@@ -72,7 +72,6 @@ fn serv_event(input: &mut FuzzInput, ev: Event, state: &mut State) -> Result<()>
             }
             if input.chance(0.9)? {
                 h.allow()?;
-                state.authed = true;
             } else if input.chance(0.4)? {
                 h.reject()?;
             }
@@ -88,11 +87,7 @@ fn serv_event(input: &mut FuzzInput, ev: Event, state: &mut State) -> Result<()>
                 h.enable_pubkey_auth(false).unwrap();
             }
             if input.chance(0.9)? {
-                let real = h.real();
                 h.allow()?;
-                if real {
-                    state.authed = true;
-                }
             } else if input.chance(0.4)? {
                 h.reject()?;
             }

src/event.rs
@@ -273,7 +273,9 @@ pub enum ServEvent<'g, 'a> {
     ///
     /// Note that this event may be emitted multiple times,
     /// since the client first queries acceptable public keys,
-    /// and then later sends an actual signature.
+    /// and then later sends an actual signature. The application
+    /// should give consistent `allow()`/`deny()` (default) responses
+    /// for requests of the same key.
     PubkeyAuth(ServPubkeyAuth<'g, 'a>),
     /// Authentication success.
     ///
@@ -476,14 +478,6 @@ impl<'g, 'a> ServPubkeyAuth<'g, 'a> {
     /// Retrieve the public key presented by a client.
     pub fn pubkey(&self) -> Result<PubKey<'_>> {
         self.runner.fetch_servpubkey()
-    }
-
-    /// Whether this is an pubkey auth attempt.
-    ///
-    /// `real()` will be `false` for a pubkey key query (no signature attemp),
-    /// or `true` for the actual login attempt with signature.
-    pub fn real(&self) -> bool {
-        self.real_sig
     }
 
     /// Accept the presented public key.

commit 578e1aec57f6c26bc524ab0db68048277639d95c

fuzz: fix fuzz-server since removing pubkey real
It isn't valid to set "authed" for a pubkey success, since it might be a
test authentication. Instead just use the Authenticated event.

Fixes: 73a8142b4302 ("Remove ServPubkeyAuth::real()")

fuzz/src/server.rs
@@ -71,6 +71,7 @@ fn serv_event(input: &mut FuzzInput, ev: Event, state: &mut State) -> Result<()>
                 h.enable_pubkey_auth(false).unwrap();
             }
             if input.chance(0.9)? {
+                state.authed = true;
                 h.allow()?;
             } else if input.chance(0.4)? {
                 h.reject()?;
@@ -87,13 +88,13 @@ fn serv_event(input: &mut FuzzInput, ev: Event, state: &mut State) -> Result<()>
                 h.enable_pubkey_auth(false).unwrap();
             }
             if input.chance(0.9)? {
+                println!("fuzz allow pub");
                 h.allow()?;
             } else if input.chance(0.4)? {
                 h.reject()?;
             }
         }
         ServEvent::Authenticated => {
-            assert!(state.authed);
             state.authed = true;
         }
         ServEvent::OpenSession(h) => {