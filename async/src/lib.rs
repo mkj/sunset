49f278bcda24859df979088a20e173a79e1bd62e
Matt Johnston <matt@ucc.asn.au>
2026-06-04 22:37:53 +0800

    async: update docs

--- async/src/lib.rs
+++ async/src/lib.rs
@@ -1,6 +1,15 @@
-//! Async for Sunset SSH
+//! Async for [Sunset SSH](sunset)
 //!
-//! This provides async for Sunset SSH.
+//! [`SSHClient`] and [`SSHServer`] provide async-executor agnostic SSH
+//! implementations. These can be used on full-sized async platforms
+//! (Tokio, smol, etc) as well as on `no_std` embedded platforms such as
+//! [`embassy-executor`](https://docs.rs/embassy-executor).
+//!
+//! On std platforms some higher level functionality is in
+//! `sunset-stdasync` crate.
+//! [`embedded-io-adapters`](https://docs.rs/embedded-io-adapters)
+//! can be used for `Read` or `Write` traits with different async runtimes.
+
 #![cfg_attr(not(any(feature = "std", test)), no_std)]
 #![forbid(unsafe_code)]
 // avoid mysterious missing awaits
@@ -11,8 +20,6 @@ mod async_sunset;
 mod client;
 mod server;
 
-// TODO: if SSHServer and SSHClient don't specialise much then
-// they could share a common implementation. Wait and see
 pub use client::SSHClient;
 pub use server::SSHServer;
 
