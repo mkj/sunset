mkj/sunset

commit 49dbe754f084ab1fcb98a19564a3a8236ed5b43a

Re-export signing key crates
Signing/Verifying types are part of the API, applications might want to
handle keys externally.

The demo config code needs that.

src/lib.rs
@@ -72,3 +72,14 @@ extern crate self as sunset;
 
 #[cfg(feature = "embedded-io")]
 pub use embedded_io;
+
+// Re-exports.
+// Crypto formats so that applications can handle
+// private keys themselves.
+#[cfg(feature = "_ecdsa")]
+use ecdsa;
+pub use ed25519_dalek;
+#[cfg(feature = "ecdsa256")]
+use p256;
+#[cfg(feature = "rsa")]
+pub use rsa;

demo/common/Cargo.toml
@@ -24,8 +24,6 @@ hmac = { version = "0.12", default-features = false }
 
 # bcrypt depends on getrandom
 bcrypt = { version = "0.15", default-features = false }
-# dalek depends on getrandom
-ed25519-dalek = { version = "2.0.0-rc.2", default-features = false }
 subtle = { version = "2.4", default-features = false }
 
 getrandom = "0.2"

demo/common/src/config.rs
@@ -7,6 +7,7 @@ use log::{debug, error, info, log, trace, warn};
 use hmac::{Hmac, Mac};
 use sha2::Sha256;
 use subtle::ConstantTimeEq;
+use sunset::ed25519_dalek;
 
 use heapless::{String, Vec};
 

commit 1ebbac9b310ab9010833dbe2778504c21601812e

Make ecdsa re-export public
Caught by clippy.

Fixes: 49dbe754f084 ("Re-export signing key crates")

src/lib.rs
@@ -79,10 +79,12 @@ pub use embedded_io;
 // Re-exports.
 // Crypto formats so that applications can handle
 // private keys themselves.
-#[cfg(feature = "_ecdsa")]
-use ecdsa;
 pub use ed25519_dalek;
+
+#[cfg(feature = "_ecdsa")]
+pub use ecdsa;
 #[cfg(feature = "ecdsa256")]
-use p256;
+pub use p256;
+
 #[cfg(feature = "rsa")]
 pub use rsa;