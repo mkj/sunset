mkj/sunset

commit 40a35e89e76cfbaed58e6ec56f34a9f8a20e72fd

Make Error::bug() return Result::Err
That's the most common case. Error::build_bug() is added to return just
the Error.

src/error.rs
@@ -165,9 +165,10 @@ pub enum Error {
     #[cfg(feature = "embedded-io")]
     EmbeddedIoError { kind: embedded_io::ErrorKind },
 
-    // This state should not be reached, previous logic should have prevented it.
+    // Error::Bug should not be reached, previous logic should have prevented it.
     // Create this using [`Error::bug()`] or [`.trap()`](TrapBug::trap).
-    // Location is currently disabled due to bloat.
+    // Location is currently disabled due to bloat of full paths.
+    // https://github.com/rust-lang/rust/issues/95529 is having functions.
     // #[snafu(display("Program bug {location}"))]
     // Bug { location: snafu::Location },
     /// Program bug
@@ -183,23 +184,21 @@ impl Error {
 
     #[cold]
     #[track_caller]
+    /// Panics in debug builds, returns [`Result::Err(Error::Bug)`](Error::Bug) in release.
+    pub fn bug<T>() -> Result<T, Error> {
+        Err(Self::build_bug())
+    }
+
+    #[cold]
+    #[track_caller]
     /// Panics in debug builds, returns [`Error::Bug`] in release.
-    // TODO: this should return a Result since it's always used as Err(Error::bug())
-    pub fn bug() -> Error {
+    pub fn build_bug() -> Error {
         // Easier to track the source of errors in development,
         // but release builds shouldn't panic.
         if cfg!(debug_assertions) {
             panic!("Hit a bug");
         } else {
-            // let caller = core::panic::Location::caller();
             Error::Bug
-            // {
-            //     location: snafu::Location::new(
-            //         caller.file(),
-            //         caller.line(),
-            //         caller.column(),
-            //     ),
-            // }
         }
     }
 
@@ -207,15 +206,13 @@ impl Error {
     ///
     /// The message can be used instead of a code comment, is logged at `trace` level.
     #[cold]
-    pub fn bug_fmt(args: Arguments) -> Error {
+    fn bug_fmt(args: Arguments) -> Error {
         // Easier to track the source of errors in development,
         // but release builds shouldn't panic.
         if cfg!(debug_assertions) {
             panic!("Hit a bug: {args}");
         } else {
             trace!("Hit a bug: {args}");
-            // TODO: this bloats binaries with full paths
-            // https://github.com/rust-lang/rust/issues/95529 is having function
             // let caller = core::panic::Location::caller();
             Error::Bug
             // {
@@ -229,13 +226,12 @@ impl Error {
     }
 
     #[cold]
-    /// TODO: is the generic `T` going to make it bloat?
     pub fn bug_msg<T>(msg: &str) -> Result<T, Error> {
         Err(Self::bug_fmt(format_args!("{}", msg)))
     }
 
     #[cold]
-    pub fn bug_err_msg(msg: &str) -> Error {
+    pub fn build_bug_msg(msg: &str) -> Error {
         Self::bug_fmt(format_args!("{}", msg))
     }
 }
@@ -272,7 +268,7 @@ impl<T, E> TrapBug<T> for Result<T, E> {
     #[track_caller]
     fn trap(self) -> Result<T, Error> {
         // call directly so that Location::caller() works
-        if let Ok(i) = self { Ok(i) } else { Err(Error::bug()) }
+        if let Ok(i) = self { Ok(i) } else { return Error::bug() }
     }
     #[track_caller]
     fn trap_msg(self, args: Arguments) -> Result<T, Error> {
@@ -285,7 +281,7 @@ impl<T> TrapBug<T> for Option<T> {
     #[track_caller]
     fn trap(self) -> Result<T, Error> {
         // call directly so that Location::caller() works
-        if let Some(i) = self { Ok(i) } else { Err(Error::bug()) }
+        if let Some(i) = self { Ok(i) } else { Error::bug() }
     }
     #[track_caller]
     fn trap_msg(self, args: Arguments) -> Result<T, Error> {

async/src/async_sunset.rs
@@ -69,11 +69,13 @@ impl<'a, CS: CliServ> Inner<'a, CS> {
     ///
     /// Returns split references that will be required by many callers
     fn fetch(&mut self, num: ChanNum) -> Result<(&mut Runner<'a, CS>, &ChanHandle)> {
-        let h = self
+        let ch = self
             .chan_handles
             .get(num.0 as usize)
-            .ok_or(Error::BadChannel { num })?;
-        h.as_ref().map(|ch| (&mut self.runner, ch)).ok_or_else(Error::bug)
+            .ok_or(Error::BadChannel { num })?
+            .as_ref()
+            .trap()?;
+        Ok((&mut self.runner, ch))
     }
 }
 

async/src/client.rs
@@ -50,7 +50,7 @@ impl<'a> SSHClient<'a> {
             Event::Cli(x) => Ok(x),
             Event::None => Ok(CliEvent::PollAgain),
             Event::Progressed => Ok(CliEvent::PollAgain),
-            _ => Err(Error::bug()),
+            _ => Error::bug(),
         }
     }
 

async/src/server.rs
@@ -52,7 +52,7 @@ impl<'a> SSHServer<'a> {
             Event::Serv(x) => Ok(x),
             Event::None => Ok(ServEvent::PollAgain),
             Event::Progressed => Ok(ServEvent::PollAgain),
-            Event::Cli(_) => Err(Error::bug()),
+            Event::Cli(_) => Error::bug(),
         }
     }
 

sftp/src/proto.rs
@@ -850,7 +850,7 @@ macro_rules! sftpmessages {
             /// Used by a SFTP client. Does not include the length field.
             pub fn encode_request(&self, id: ReqId, s: &mut dyn SSHSink) -> SftpResult<()> {
                 if !self.sftp_num().is_request() {
-                    Err(sunset::Error::bug())?;
+                    sunset::Error::bug()?;
                 }
 
                 // packet type
@@ -870,7 +870,7 @@ macro_rules! sftpmessages {
             pub fn encode_response(&self, s: &mut dyn SSHSink) -> SftpResult<()> {
 
                 if !self.sftp_num().is_response() {
-                    Err(sunset::Error::bug())?;
+                    sunset::Error::bug()?;
                 }
 
                 Ok(self.enc(s)?)

sftp/src/sftphandler/sftphandler.rs
@@ -504,7 +504,7 @@ where
             | SftpPacket::Name(..)
             | SftpPacket::Attrs(..) => {
                 // Should have been caught by is_request() above.
-                Err(sunset::Error::bug())?;
+                sunset::Error::bug()?;
             }
         }
         Ok(())
@@ -528,7 +528,7 @@ where
             mut remaining,
         } = self.state
         else {
-            Err(sunset::Error::bug())?
+            sunset::Error::bug()?
         };
         self.state = HandlerState::Normal;
 

sftp/src/sftpsource.rs
@@ -247,7 +247,9 @@ impl<'a> SftpSource<'a> {
     pub fn decode(&self) -> SftpDecoded<'_> {
         let Some(req_id) = self.peek_packet_req_id() else {
             // Packet is shorter than minimum.
-            return SftpDecoded::FillError { error: sunset::Error::bug().into() };
+            return SftpDecoded::FillError {
+                error: sunset::Error::build_bug().into(),
+            };
         };
 
         // Skip the length prefix

src/channel.rs
@@ -177,7 +177,7 @@ impl Channels {
                 send.max_packet,
                 send.window
             );
-            return Err(Error::bug());
+            return Error::bug();
         }
         send.window -= data.len();
         trace!("send_data: new window {}", send.window);
@@ -546,7 +546,7 @@ impl Channels {
                 Ok(())
             }
         } else {
-            Err(Error::bug())
+            Error::bug()
         }
     }
 
@@ -561,7 +561,7 @@ impl Channels {
                     ChannelReqType::Subsystem(packets::Subsystem { subsystem: command }),
                 ..
             }) => Ok(*command),
-            _ => Err(Error::bug()),
+            _ => Error::bug(),
         }
     }
 
@@ -571,7 +571,7 @@ impl Channels {
                 req: ChannelReqType::Environment(packets::Environment { name, .. }),
                 ..
             }) => Ok(*name),
-            _ => Err(Error::bug()),
+            _ => Error::bug(),
         }
     }
 
@@ -582,7 +582,7 @@ impl Channels {
                     ChannelReqType::Environment(packets::Environment { name: _, value }),
                 ..
             }) => Ok(*value),
-            _ => Err(Error::bug()),
+            _ => Error::bug(),
         }
     }
 }
@@ -1226,7 +1226,7 @@ impl<'g> CliSessionExit<'g> {
                 req: ChannelReqType::ExitSignal(e),
                 ..
             }) => Ok(Self::Signal(e.clone())),
-            _ => Err(Error::bug()),
+            _ => Error::bug(),
         }
     }
 }

commit e087ea3d68b0dbaa302f84a98072a28b6f83628d

Fix clippy return warning
Fixes: 40a35e89e76c ("Make Error::bug() return Result::Err")

src/error.rs
@@ -268,7 +268,7 @@ impl<T, E> TrapBug<T> for Result<T, E> {
     #[track_caller]
     fn trap(self) -> Result<T, Error> {
         // call directly so that Location::caller() works
-        if let Ok(i) = self { Ok(i) } else { return Error::bug() }
+        if let Ok(i) = self { Ok(i) } else { Error::bug() }
     }
     #[track_caller]
     fn trap_msg(self, args: Arguments) -> Result<T, Error> {