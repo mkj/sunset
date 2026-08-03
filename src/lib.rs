mkj/sunset

commit 08f1c40dc4fef78fdcd92d0bed039d1b6e705e39

Fix typo and pub mod ordering in sunset lib.rs

src/lib.rs
@@ -22,10 +22,9 @@ pub mod packets;
 pub mod sshnames;
 pub mod sshwire;
 // exported so that some Channel error variants can be created with .fail().
-// perhaps the ones of interest should be expored separately.
+// perhaps the ones of interest should be exported separately.
 pub mod error;
 pub mod namelist;
-mod random;
 
 pub mod event;
 
@@ -35,6 +34,7 @@ mod ident;
 mod kex;
 mod sign;
 mod test;
+mod random;
 
 mod cliauth;
 mod client;

commit 62b98f3996c88cf920cac1653e1c9e15ddf0d7d0

Fix mod sort formatting
Fixes: 08f1c40dc4fe ("Fix typo and pub mod ordering in sunset lib.rs")

src/lib.rs
@@ -32,9 +32,9 @@ mod conn;
 mod encrypt;
 mod ident;
 mod kex;
+mod random;
 mod sign;
 mod test;
-mod random;
 
 mod cliauth;
 mod client;